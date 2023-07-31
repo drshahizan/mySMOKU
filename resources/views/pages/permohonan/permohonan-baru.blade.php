dea3dbad7fe65f726fed1507f3bd813bc193ab4e
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-07-31 10:25:43 +0800

    Update permohonan-baru.blade.php

--- resources/views/pages/permohonan/permohonan-baru.blade.php
+++ resources/views/pages/permohonan/permohonan-baru.blade.php
@@ -763,7 +763,7 @@
 															<span class="indicator-progress">Please wait...
 															<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
 														</button>
-														<button type="button" class="btn btn-lg btn-primary" data-kt-stepper-action="next">Continue
+														<button type="button" class="btn btn-lg btn-primary" data-kt-stepper-action="next">Teruskan
 														<i class="ki-duotone ki-arrow-right fs-4 ms-1 me-0">
 															<span class="path1"></span>
 															<span class="path2"></span>
