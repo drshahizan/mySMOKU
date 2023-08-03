a93b0e3351eaf85d796081cc4440ebe7d515fa60
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-08-03 18:30:42 +0800

    Update maklumatProfilDiri.blade.php

--- resources/views/pages/saringan/maklumatProfilDiri.blade.php
+++ resources/views/pages/saringan/maklumatProfilDiri.blade.php
@@ -7,269 +7,282 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta property="og:type" content="article"/>
+    <link rel="stylesheet" href="assets/css/saringan.css">
 
 <body>
-    <table>
+    <table class="profile-form">
         <tr>
-            <td class="align-right">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/JATA_NEGARA_MALAYSIA.png/776px-JATA_NEGARA_MALAYSIA.png?20220224030856" alt="jata-negara-malaysia" width="50px">
-                BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)
+            <td class="text-center" colspan="3">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/JATA_NEGARA_MALAYSIA.png/776px-JATA_NEGARA_MALAYSIA.png?18218224030856" alt="jata-negara-malaysia">
+                <br><b class="title"> MAKLUMAT PROFIL DIRI</b>
+                <p><b class="description"> BANTUAN KEWANGAN ORANG KURANG UPAYA <br> (BKOKU)</b> </p> 
+                <br><br><br>
             </td>
         </tr>
         <tr>
-            <td class="header-part" colspan="3"> A.Semakan Kelayakan MQA</td>
+            <td class="header-part" colspan="3"> A. SEMAKAN KELAYAKAN MQA</td>
         </tr>
+        <div>
+            <tr class="gap-left">
+                <td class="gap-top" style="width: 16%; padding-left: 20px;">Nama Institusi</td>
+                <td class="gap-top" style="width: 2%">:</td>
+                <td class="gap-top">Universiti Malaysia Sabah</td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Peringkat</td>
+                <td style="width: 2%">:</td>
+                <td>Diploma</td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Nama Kursus</td>
+                <td style="width: 2%">:</td>
+                <td>Komputer Sains</td>
+            </tr>
+            <tr class="gap-left">
+                <td class="gap-bottom" style="width: 16%">Adakah anda penerima HLP?</td>
+                <td class="gap-bottom" style="width: 2%">:</td>
+                <td class="gap-bottom">Ya</td>
+            </tr>
+        </div>
         <tr>
-            <td>Nama Institusi</td>
-            <td style="width: 10%">:</td>
-            <td>Universiti Malaysia Sabah</td>
+            <td class="header-part" colspan="3">B. MAKLUMAT PERIBADI</td>
         </tr>
+        <div>
+            <tr class="gap-left">
+                <td style="width: 16%" class="gap-top">Nama</td>
+                <td style="width: 2%" class="gap-top">:</td>
+                <td class="gap-top"></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">No. Kad Pengenalan</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Tarikh Lahir</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Umur</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Jantina</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td>No. JKM</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Kecacatan</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Bangsa</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Alamat Rumah</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Poskod</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Bandar</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Negeri</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">No. Tel(HP)</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">No. Tel Rumah</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td style="width: 16%">Alamat Emel</td>
+                <td style="width: 2%">:</td>
+                <td></td>
+            </tr>
+            <tr class="gap-left">
+                <td class="gap-bottom" style="width: 16%">No. Akaun Bank</td>
+                <td class="gap-bottom" style="width: 2%">:</td>
+                <td class="gap-bottom"></td>
+            </tr>
+        </div>
         <tr>
-            <td>Peringkat</td>
-            <td style="width: 10%">:</td>
-            <td>Diploma</td>
+            <td class="header-part" colspan="3">C. MAKLUMAT WARIS</td>
         </tr>
-        <tr>
-            <td>Nama Kursus</td>
-            <td style="width: 10%">:</td>
-            <td>Komputer Sains</td>
+        <div>
+        <tr class="gap-left">
+            <td style="width: 16%" class="gap-top">Nama </td>
+            <td style="width: 2%" class="gap-top">:</td>
+            <td class="gap-top"></td>
         </tr>
-        <tr>
-            <td>Adakah anda penerima HLP?</td>
-            <td style="width: 10%">:</td>
-            <td>Ya</td>
-        </tr>
-        <tr>
-            <td class="header-part" colspan="3">B. Maklumat Peribadi</td>
-        </tr>
-        <tr>
-            <td>Nama </td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">No. Kad Pengenalan</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. Kad Pengenalan</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">No Pasport</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Tarikh Lahir</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Hubungan Waris</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Umur</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Lain-lain (Sila Nyatakan)</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Jantina</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Alamat Rumah</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. JKM</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Poskod</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Kecacatan</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Bandar</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Bangsa</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Negeri</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Alamat Rumah</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">No. Tel(HP)</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Poskod</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">No. Tel Rumah</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
+        <tr class="gap-left">
+            <td class="gap-bottom" style="width: 16%">Pendapatan</td>
+            <td class="gap-bottom" style="width: 2%">:</td>
+            <td class="gap-bottom"></td>
+        </tr>
+    </div>
         <tr>
-            <td>Bandar</td>
-            <td style="width: 10%">:</td>
+            <td class="header-part" colspan="3">D. MAKLUMAT AKADEMIK</td>
+        </tr>
+    <div>
+        <tr class="gap-left">
+            <td style="width: 16%" class="gap-top">No Pendaftaran Pelajar</td>
+            <td style="width: 2%" class="gap-top">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Negeri</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Nama Kursus</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. Tel(HP)</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Peringkat Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. Tel Rumah</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Nama Pusat Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Alamat Emel</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Tarikh Mula Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. Akaun Bank</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Tarikh Tamat Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr></tr>
-            <td class="header-part" colspan="3">C. Maklumat Waris</td>
-        </tr>
-        <tr>
-            <td>Nama </td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Semester Semasa</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No. Kad Pengenalan</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Tempoh Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>No Pasport</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Bil Bulan Persemester</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Hubungan Waris</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Mod Pengajian</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Lain-lain (Sila Nyatakan)</td>
-            <td style="width: 10%">:</td>
+        <tr class="gap-left">
+            <td style="width: 16%">Sumber Pembiayaan</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
+        <tr class="gap-left">
+            <td class="gap-bottom" style="width: 16%">Nama Penaja</td>
+            <td class="gap-bottom" style="width: 2%">:</td>
+            <td class="gap-bottom"></td>
+        </tr>
+    </div>
         <tr>
-            <td>Alamat Rumah</td>
-            <td style="width: 10%">:</td>
+            <td class="header-part" colspan="3">E. MAKLUMAT TUTUTAN</td>
+        </tr>
+    <div>
+        <tr class="gap-left">
+            <td style="width: 16%" class="gap-top">Jenis Tuntutan</td>
+            <td style="width: 2%">:</td>
             <td></td>
         </tr>
-        <tr>
-            <td>Poskod</td>
-            <td style="width: 10%">:</td>
-            <td></td>
+        <tr class="gap-left">
+            <td class="gap-bottom" style="width: 16%">Amaun</td>
+            <td class="gap-bottom" style="width: 2%">:</td>
+            <td class="gap-bottom"></td>
         </tr>
-        <tr>
-            <td>Bandar</td>
-            <td style="width: 10%">:</td>
-            <td></td>
         </tr>
-        <tr>
-            <td>Negeri</td>
-            <td style="width: 10%">:</td>
-            <td></td>
+            <td class="header-part" colspan="3">F. PERAKUAN</td>
         </tr>
-        <tr>
-            <td>No. Tel(HP)</td>
-            <td style="width: 10%">:</td>
-            <td></td>
+        <tr class="gap-left">
+            <td colspan="3" class="gap-top gap-bottom">
+                Saya mengaku bahawa segala maklumat yang diberikan adalah betul dan benar belaka. Saya juga faham
+                sekiranya maklumat yang diberikan didapati palsu atau tidak benar, pihak kementerian berhak menolak
+                permohonan saya dan menghentikan bantuan kewangan ini kepada saya.
+            </td>
         </tr>
-        <tr>
-            <td>No. Tel Rumah</td>
-            <td style="width: 10%">:</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Pendapatan</td>
-            <td style="width: 10%">:</td>
-            <td></td>
-        </tr>
-    </tr>
-        <td class="header-part" colspan="3">D. Maklumat Akademik</td>
-    </tr>
-    <tr>
-        <td>No Pendaftaran Pelajar</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Nama Kursus</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Peringkat Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Nama Pusat Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Tarikh Mula Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Tarikh Tamat Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Semester Semasa</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Tempoh Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Bil Bulan Persemester</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Mod Pengajian</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Sumber Pembiayaan</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Nama Penaja</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    </tr>
-        <td class="header-part" colspan="3">E. Maklumat Tuntutan</td>
-    </tr>
-    <tr>
-        <td>Jenis Tuntutan</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Amaun</td>
-        <td style="width: 10%">:</td>
-        <td></td>
-    </tr>
-    </tr>
-        <td class="header-part" colspan="3">F. Perakuan</td>
-    </tr>
-    <tr>
-        <td colspan="3">
-            Saya mengaku bahawa segala maklumat yang diberikan adalah betul dan benar belaka. Saya juga faham
-            sekiranya maklumat yang diberikan didapati palsu atau tidak benar, pihak kementerian berhak menolak
-            permohonan saya dan menghentikan bantuan kewangan ini kepada saya
-        </td>
-    </tr>
+    </div>
 </body>
 </html>
