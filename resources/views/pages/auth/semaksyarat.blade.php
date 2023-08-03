8ce4e96fc1d57db34fbe4f5a79491f8969c9b58a
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-03 13:00:06 +0800

    push

--- resources/views/pages/auth/semaksyarat.blade.php
+++ resources/views/pages/auth/semaksyarat.blade.php
@@ -12,18 +12,39 @@
             <!--end::Title-->
             <br>
             <br>
-            <!--begin::Subtitle-->
-            <h2 class="text-dark fw-bolder mb-3">
-                Semakan Sistem Maklumat Orang Kurang Upaya (SMOKU)
-            </h2>
-            <!--end::Subtitle--->
+            <h3 class="text-dark fw-bolder mb-3">
+                Semakan Kelayakan
+            </h3>
         </div>
+        <div class="fv-row mb-10">
+			<label class="form-label required">Adakah anda penerima HLP?</label>	
+			<div class="row mb-2" data-kt-buttons="true">
+			<!--begin::Col-->
+			<div class="col">
+			<!--begin::Option-->
+				<label class="btn btn-outline btn-outline-dashed btn-active-light-primary w-100 p-4">
+					<input type="radio" class="btn-check" name="ya" value="ya" />
+							<span class="fw-bold fs-3">Ya</span></label><!--end::Option-->
+																	</div>
+																	<!--end::Col-->
+																	<!--begin::Col-->
+																	<div class="col">
+																		<!--begin::Option-->
+																		<label class="btn btn-outline btn-outline-dashed btn-active-light-primary w-100 p-4 active">
+																			<input type="radio" class="btn-check" name="" checked="checked" value="tidak" />
+																			<span class="fw-bold fs-3">Tidak</span>
+																		</label>
+																		<!--end::Option-->
+																	</div>
+																</div>
+															
+														</div>
         <!--begin::Heading-->
 
         <div class="text-center mb-11">
             <!--begin::Title-->
             <!-- <h1 class="text-dark fw-bolder mb-3" > -->
-            <img alt="Logo" src="{{ image('logos/logo.png') }}" class="h-100px h-lg-90px"/>
+            <img alt="Logo" src="{{ image('logos/mqalogo.png') }}" class="h-100px h-lg-90px"/>
           
                 
            
