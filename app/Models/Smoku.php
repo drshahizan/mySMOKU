be146cf9fd11ebabae882494108c660639e2bd7b
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-06 04:12:05 +0800

    Create Smoku.php

---
+++ app/Models/Smoku.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Smoku extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
+    protected $table = 'smoku';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nokp',
+        'email',
+        'last_login_at',
+        'last_login_ip',
+    ];
+
+    public function id()
+    {
+        return $this->nokp;
+    }
+}
