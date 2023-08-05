414fdef6e14c581f4b9ea4e82d35e55ecb43d755
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-06 04:15:50 +0800

    Create 2023_08_06_000000_create_smoku_table.php

---
+++ database/migrations/2023_08_06_000000_create_smoku_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('smoku', function (Blueprint $table) {
+            $table->id();
+            $table->string('nokp')->unique();
+            $table->string('nama');
+            $table->integer('verify');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('smoku');
+    }
+};
