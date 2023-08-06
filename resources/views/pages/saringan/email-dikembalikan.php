636d11189a6a0a2a6fb299a409d96100196d57e6
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-08-06 13:46:59 +0800

    Update email-dikembalikan.php

--- resources/views/pages/saringan/email-dikembalikan.php
+++ resources/views/pages/saringan/email-dikembalikan.php
@@ -7,7 +7,7 @@
     <h3>BKOKU: PERMOHONAN DIKEMBALIKAN</h3>
     <p>Assalammualaikum W.R.T & Salam Sejahtera.</p>
     <p>Dukacita dimaklumkan bahawa permohonan anda dikembalikan semula atas alasan yang dinyatakan di bawah:</p>
-    <p>1. Terdapat maklumat yag tidak benar dalam Profil Pemohon</p>
+    <p>1. Terdapat butiran yang tidak benar dalam Profil Pemohon</p>
     <p>2. Terdapat butiran yang tidak benar dalam Maklumat Akademik</p>
     <p>3. Terdapat butiran yang tidak benar dalam Salinan Dokumen</p>
     <br>
