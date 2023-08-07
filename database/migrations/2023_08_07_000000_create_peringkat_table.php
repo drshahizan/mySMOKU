a08ccc77e583551a2f49d46a3fb32b327378d80a
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-08 03:03:30 +0800

    Create 2023_08_07_000000_create_peringkat_table.php

---
+++ database/migrations/2023_08_07_000000_create_peringkat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bk_peringkatpengajian', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kodperingkat');
+            $table->string('peringkat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bk_peringkatpengajian');
+    }
+};
