0e68bd3d7b61c05329e3f85657e0e557e4a3bb7e
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-08 03:13:45 +0800

    Update SemakUserController.php

--- app/Http/Controllers/Auth/SemakUserController.php
+++ app/Http/Controllers/Auth/SemakUserController.php
@@ -7,28 +7,54 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Infoipt;
+use App\Models\PeringkatPengajian;
+use App\Models\Kursus;
+//use DB;
 
 class SemakUserController extends Controller
 {
     public function create()
     {
-        addJavascriptFile('assets/js/custom/authentication/semak/general.js');
+        $ipt = Infoipt::all()->sortBy('namaipt');
+        $peringkat = PeringkatPengajian::all()->sortBy('kodperingkat');
+        $kursus = Kursus::all()->sortBy('nama_kursus');
 
-        return view('pages.auth.semaksyarat');
+        return view('pages.auth.semaksyarat', compact('ipt','peringkat','kursus'));
+
     }
     public function store(Request $request)
     {
-        //$request->authenticate();
-        $request->session()->regenerate();
-        //return redirect()->intended(RouteServiceProvider::HOME);
-        /*$request->validate([
-            'terimaHLP' => ['required', 'string'],
-            'cuti' => ['required', 'string'],
-            'id_institusi' => ['required', 'string'],
-            'peringkat_pengajian' => ['required', 'string'],
-            'nama_kursus' => ['required', 'string'],
+        $request->validate([
+            'terimHLP' => ['required'],
+            'cuti' => ['required'],
+            'id_institusi' => ['required'],
+            'peringkat_pengajian' => ['required'],
+            'nama_kursus' => ['required'],
             
-        ]);*/
+        ]);
+
+            $terimHLP = $request->terimHLP;
+            $cuti = $request->cuti;
+            $id_institusi = $request->id_institusi;
+            $peringkat_pengajian = $request->peringkat_pengajian;
+            $nama_kursus = $request->nama_kursus;
+
+            
+            if ($terimHLP == 'ya') {
+                return redirect()->route('login')
+                ->with('message', 'Anda tidak layak daftar kerana anda penerima HLP');
+                
+            } else {
+
+                /*DB::table('smoku')->where('nokp' ,$request->nokp)->update([
+
+                'verify'=>'1'
+    
+                ]);*/
+
+                return redirect()->route('daftarlayak');
+            }
 
         
 
