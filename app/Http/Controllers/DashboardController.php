a15719d922beb8d6f782c1efec4e9dae50f875af
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-10 03:29:28 +0800

    pushhhhhhhhhhhhh

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Permohonan;
+use App\Models\Status;
+use App\Models\Akademik;
+use App\Models\TuntutanPermohonan;
 
 class DashboardController extends Controller
 {
@@ -10,17 +14,25 @@ class DashboardController extends Controller
     {
         addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
 
-        //return view('pages.dashboards.index');
-        /*if (Auth::check())
-        {
-            $id = Auth::user()->tahap();
-            return $id;
-        }*/
+        $pelajar = Permohonan::all()->where('nokp_pelajar', Auth::user()->id());
+        $status = Status::leftJoin('statusinfo','statusinfo.kodstatus','=','statustransaksi.status')
+        ->get(['statustransaksi.*', 'statusinfo.*'])
+        ->where('nokp_pelajar', Auth::user()->id());
+        $akademik = Akademik::all()
+        //leftJoin('bk_peringkatpengajian','bk_peringkatpengajian.kodperingkat','=','maklumatakademik.peringkat_pengajian')
+        //->get(['maklumatakademik.*', 'bk_peringkatpengajian.*'])
+        ->where('nokp_pelajar', Auth::user()->id());
+        $sem = Akademik::
+        leftJoin('bk_peringkatpengajian','bk_peringkatpengajian.kodperingkat','=','maklumatakademik.peringkat_pengajian')
+        ->get(['maklumatakademik.*', 'bk_peringkatpengajian.*'])
+        ->where('nokp_pelajar', Auth::user()->id());
+        $tuntutanpermohonan = TuntutanPermohonan::all()->where('nokp_pelajar', Auth::user()->id());
+
 
 
         if(Auth::user()->tahap=='1')
         {
-            return view('pages.dashboards.index')->with('message', 'Selamat Datang ke Laman Utama Pelajar');
+            return view('pages.dashboards.index', compact('pelajar','status','akademik','sem','tuntutanpermohonan'))->with('message', 'Selamat Datang ke Laman Utama Pelajar');
         }
         else if(Auth::user()->tahap=='2')
         {
