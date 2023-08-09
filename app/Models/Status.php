da5afa8b52b1a2078e1309031f38e4018c3411db
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-09 23:22:09 +0800

    Create Status.php

---
+++ app/Models/Status.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Status extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
+    protected $table = 'statustransaksi';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_permohonan',
+        //'id_tuntutan',
+        'status',
+    ];
+
+}
