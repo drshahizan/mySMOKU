4f990b17e00f22239a15c524adb0765b58b17799
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-08-09 13:00:02 +0800

    Update salinanDokumen.blade.php

--- resources/views/pages/saringan/salinanDokumen.blade.php
+++ resources/views/pages/saringan/salinanDokumen.blade.php
@@ -1,82 +1,69 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {!! printHtmlAttributes('html') !!}>
-<!--begin::Head-->
-<head>
-    <title>{{ config('app.name', 'SistemBKOKU') }}</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta property="og:type" content="article"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</head>
-<body>
+<x-default-layout><body>
     <br>
-    <h3 style="text-align: center">SALINAN DOKUMEN</h3>
+    <h3>Salinan Dokumen Pemohon</h3>
     <br>
 <!--begin::Accordion-->
 <div class="accordion" id="accordionPanelsStayOpenExample">
     <div class="accordion-item">
       <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-          <b>Surat Tawaran</b> 
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+          <b style="color: black!important">Surat Tawaran</b> 
         </button>
       </h2>
       <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
         <div class="accordion-body" style="text-align: center">
-            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="700px"/>
+            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="650px"/>
         </div>
       </div>
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-          <b>No. Akaun Bank Islam</b> 
+          <b style="color: black!important">No. Akaun Bank Islam</b> 
         </button>
       </h2>
       <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
         <div class="accordion-body" style="text-align: center">
-            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="700px"/>
+            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="650px"/>
         </div>
       </div>
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="panelsStayOpen-headingThree">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-          <b>Keputusan Peperiksaan</b> 
+          <b style="color: black!important">Keputusan Peperiksaan</b> 
         </button>
       </h2>
       <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
         <div class="accordion-body" style="text-align: center">
-            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="700px"/>
+            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="650px"/>
         </div>
       </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="panelsStayOpen-headingFour">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
-            <b>Invois/Resit</b> 
+            <b style="color: black!important">Invois/Resit</b> 
           </button>
         </h2>
         <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
           <div class="accordion-body" style="text-align: center">
-            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="700px"/>
+            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="650px"/>
           </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="panelsStayOpen-headingFive">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
-            <b>Maklumat Bank IPTA/IPTS/Kolej Komuniti/Politeknik Atau Pengesahan Semester Pengajian</b> 
+            <b style="color: black!important">Maklumat Bank IPTA/IPTS/Kolej Komuniti/Politeknik Atau Pengesahan Semester Pengajian</b> 
           </button>
         </h2>
         <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
           <div class="accordion-body" style="text-align: center">
-            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="700px"/>
+            <embed src="assets/pdf/SURAT TAWARAN BKOKU.pdf" width="90%" height="650px"/>
           </div>
         </div>
     </div>
 </div>
 <!--end::Accordion-->
-</body>
-</html>
+</x-default-layout>
