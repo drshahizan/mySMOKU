d240302780d0cf3e320dc760c02f7291d8238b71
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-09 10:25:47 +0800

    Update _footer.blade.php

--- resources/views/layout/partials/sidebar-layout/_footer.blade.php
+++ resources/views/layout/partials/sidebar-layout/_footer.blade.php
@@ -3,9 +3,9 @@
 	<!--begin::Footer container-->
 	<div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
 		<!--begin::Copyright-->
-		<div class="text-dark order-2 order-md-1 center">
-			<span class="text-muted fw-semibold me-1">{{ date('Y') }}&copy;</span>
-			<a href="#" target="_blank" class="text-gray-800 text-hover-primary">KPT</a>
+		<div class="text-dark order-2 order-md-1" style="text-align: center">
+			<span class="text-muted fw-semibold me-1">Portal Rasmi Kementerian Pendidikan Tinggi {{ date('Y') }}&copy;</span>
+			<a href="#" target="_blank" class="text-gray-800 text-hover-primary">Hak Cipta Terpelihara</a>
 		</div>
 		<!--end::Copyright-->
 	</div>
