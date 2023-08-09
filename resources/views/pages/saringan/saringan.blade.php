a58e6013cd18f4291e5cd202df18cdf9e9e776a4
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-08-09 22:39:13 +0800

    Update saringan.blade.php

--- resources/views/pages/saringan/saringan.blade.php
+++ resources/views/pages/saringan/saringan.blade.php
@@ -24,67 +24,17 @@
                 </div>
             </div>        
             <div class="row clearfix">
-                <div class="col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center">
-                                <div class="icon-in-bg bg-indigo text-white rounded-circle"><i class="fa fa-file-text"></i></div>
-                                <div class="ml-4">
-                                    <span>Jumlah Saringan</span>
-                                    <h4 class="mb-0 font-weight-medium">5000</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center">
-                                <div class="icon-in-bg bg-azura text-white rounded-circle"><i class="fa fa-search"></i></div>
-                                <div class="ml-4">
-                                    <span>Belum Disemak</span>
-                                    <h4 class="mb-0 font-weight-medium">4992</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center">
-                                <div class="icon-in-bg bg-orange text-white rounded-circle"><i class="fa fa-check"></i></div>
-                                <div class="ml-4">
-                                    <span>Disokong</span>
-                                    <h4 class="mb-0 font-weight-medium">8</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center">
-                                <div class="icon-in-bg bg-pink text-white rounded-circle"><i class="fa fa-reply-all"></i></div>
-                                <div class="ml-4">
-                                    <span>Dikembalikan</span>
-                                    <h4 class="mb-0 font-weight-medium">259</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row clearfix">
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="header">
                             <h2>Senarai Saringan Permohonan<br><small>Tekan ID Permohonan untuk melakukan saringan selanjutnya</small></h2>
                             <ul class="header-dropdown dropdown">
-                                <li><a href="{{ url('cetak-senarai-pemohon') }}" target="_blank"><i class="fa fa-print"></i></a></li>
+                                <li>
+                                    <a href="{{ url('cetak-senarai-pemohon') }}" target="_blank" class="btn btn-primary" style="color: white">
+                                        <i class="fa fa-print" style="color: white!important"></i> 
+                                        Cetak Senarai Pendek
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                         <div class="body">
@@ -105,93 +55,93 @@
                                             <td><a href="{{ url('maklumat-pemohon'. $row['id_permohonan']) }}" title="">SARJANABKOKU000011</a></td>
                                             <td>BKOKU</td>
                                             <td>7/7/2023</td>
-                                            <td><button type="button" class="btn btn-danger"> Belum Disemak </button></td>
+                                            <td><button class="btn btn-danger"> Belum Disaring </button></td>
                                         </tr> --}}
                                         <tr>                                            
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUB990404080221</a></td>
                                             <td>Mohd Ali Bin Abu Kassim</td>
                                             <td>BKOKU</td>
                                             <td class="text-center">7/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn btn-danger"> Belum Disemak </button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring </button></td>
                                         </tr>
                                         <tr>                                            
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKB970204052445</a></td>
                                             <td>Sarah Binti Yusri</td>
                                             <td>PPK</td>                                        
                                             <td class="text-center">5/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button type="button" class="btn bg-orange text-white"> Sedang Disaring</button></td>
                                         </tr> <tr>                                            
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUB980112105666</a></td>
                                             <td>Aishah Binti Samsudin</td>
                                             <td>BKOKU</td>                                       
                                             <td class="text-center">2/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr> <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUM970703041223</a></td>
                                             <td>Santosh A/L Ariyaran</td>
                                             <td>BKOKU</td>                                        
                                             <td class="text-center">10/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr> <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUM960909105668</a></td>
                                             <td>Ling Kai Jie</td>
                                             <td>BKOKU</td>                                        
                                             <td class="text-center">9/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button type="button" class="btn bg-orange text-white"> Sedang Disaring</button></td>
                                         </tr> <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKM950804082447</a></td>
                                             <td>Akmal Bin Kairuddin</td>
                                             <td>PPK</td>                                        
                                             <td class="text-center">7/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKD021212050334</a></td>
                                             <td>Santishwaran A/L Paven</td>
                                             <td>PPK</td>                                        
                                             <td class="text-center">5/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKD001205034745</a></td>
                                             <td>Choo Mei Ling</td>
                                             <td>BKOKU</td>
                                             <td class="text-center">7/6/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-orange text-white"> Dikembalikan</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUP890201065225</a></td>
                                             <td>Ezra Hanisah Binti Md Yunos</td>
                                             <td>BKOKU</td>                                    
                                             <td class="text-center">9/2/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr><tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKD010305058473</a></td>
                                             <td>Arshahad Bin Kairul Zaman</td>
                                             <td>PPK</td>                                        
                                             <td class="text-center">7/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTPPKB981004045253</a></td>
                                             <td>Syed Abdul Kassim Hussain Yusof</td>
                                             <td>PPK</td>                                        
                                             <td class="text-center">5/7/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUD990201051446</a></td>
                                             <td>Shakira Mariam Aqilah Binti Syed Abdul Rahman</td>
                                             <td>BKOKU</td>                                        
                                             <td class="text-center">7/6/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-orange text-white"> Dikembalikan</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                         <tr>
                                             <td><a href="{{ url('maklumat-pemohon') }}" title="">KPTBKOKUP940524032341</a></td>
                                             <td>Rahman Mohammed Arshahad Al-dhaqm</td>
                                             <td>BKOKU</td>                                    
                                             <td class="text-center">9/2/2023</td>
-                                            <td class="text-center"><button type="button" class="btn bg-green text-white"> Disokong</button></td>
+                                            <td class="text-center"><button class="btn btn-danger"> Belum Disaring</button></td>
                                         </tr>
                                     </tbody>
                                 </table>
