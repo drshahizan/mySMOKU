581bcc5e61f7f6fa407fd413b4c100c3b7796e2d
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-10 11:39:11 +0800

    Update _navbar.blade.php

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -40,11 +40,13 @@
 	<div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
         <!--begin::Menu wrapper-->
 		<div class="cursor-pointer symbol  symbol-35px symbol-2by3" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            
+            <!-- @if(Auth::user()->profile_photo_url)
+                <img src="{{ \Auth::user()->profile_photo_url }}" class="rounded-3" alt="user" />
+            @else -->
                 <div class="symbol-label fs-6 {{ app(\App\Actions\GetThemeType::class)->handle('bg-light-? text-?', Auth::user()->nokp) }}">
                     {{ substr(Auth::user()->nokp, 0, 1) }}
                 </div>
-            
+            <!-- @endif -->
         </div>
         @include('partials/menus/_user-account-menu')
         <!--end::Menu wrapper-->
