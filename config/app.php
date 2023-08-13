084dec3ae9d5af8e4f15f8f7c3e0f7af02f51a91
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-13 14:54:47 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -185,8 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-
+        
         /*
          * Application Service Providers...
          */
