da7257f16c3ff1fa45e2d8d30154c6da3eddec75
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-14 13:04:51 +0800

    Update SemakUserController.php

--- app/Http/Controllers/Auth/SemakUserController.php
+++ app/Http/Controllers/Auth/SemakUserController.php
@@ -11,13 +11,64 @@ use App\Models\Infoipt;
 use App\Models\PeringkatPengajian;
 use App\Models\Kursus;
 use App\Models\Akademik;
-//use DB;
+use DB;
 use session;
 
 
 class SemakUserController extends Controller
 {
-    public function create(Request $request)
+
+    public function index(){
+        //$nokp = $request->session()->get('nokp');
+
+        $ipt = Infoipt::orderby("namaipt","asc")
+             ->select('idipt','namaipt')
+             ->get();
+        $kodperingkat = PeringkatPengajian::orderby("peringkat","asc")
+             ->select('kodperingkat','peringkat')
+             ->get();
+
+        // Load index view
+        return view('pages.auth.semaksyarat')
+        ->with("ipt",$ipt)
+        ->with("kodperingkat",$kodperingkat);
+   }
+
+   // Fetch records
+   public function getPeringkat($ipt=0){
+
+        // Fetch kursus by idipt
+        $peringkatData['data'] = DB::table('kursus')
+            //->join('bk_peringkatpengajian', 'kursus.kodperingkat', '=', 'bk_peringkatpengajian.kodperingkat')
+            ->select('kursus.kodperingkat','bk_peringkatpengajian.peringkat')
+            ->join('bk_peringkatpengajian', function ($join) {
+                $join->on('kursus.kodperingkat', '=', 'bk_peringkatpengajian.kodperingkat'); 
+            })
+            ->where('idipt',$ipt)
+            ->groupBy('kursus.kodperingkat','bk_peringkatpengajian.peringkat')
+
+            ->get();
+             
+
+             return response()->json($peringkatData);
+
+   }
+   public function getKursus($kodperingkat=0,$ipt=0){
+
+        // Fetch kursus by idipt
+        $kursusData['data'] = Kursus::orderby("nama_kursus","asc")
+             ->select('idipt','kodperingkat','nama_kursus')
+             ->where('kodperingkat',$kodperingkat)
+             ->where('idipt',$ipt)
+             
+             //->where(["idipt"=> $ipt, "kodperingkat" => $kodperingkat])
+             ->get();
+
+             return response()->json($kursusData);
+
+   }
+
+    /*public function create(Request $request)
     {
         $nokp = $request->session()->get('nokp');
         //dd($nokp);
@@ -28,7 +79,7 @@ class SemakUserController extends Controller
 
         return view('pages.auth.semaksyarat', compact('ipt','peringkat','kursus'));
 
-    }
+    }*/
     public function store(Request $request)
     {
         $request->validate([
@@ -51,7 +102,13 @@ class SemakUserController extends Controller
                 return redirect()->route('login')
                 ->with('message', 'Anda tidak layak daftar kerana anda penerima HLP');
                 
-            } else {
+            } else if ($cuti == 'ya') {
+                return redirect()->route('login')
+                ->with('message', 'Anda tidak layak daftar kerana anda penerima Cuti Belajar Bergaji Penuh');
+                
+            } 
+            
+            else {
 
                 $user = Akademik::create([
                     'nokp_pelajar' => $request->session()->get('nokp'),
