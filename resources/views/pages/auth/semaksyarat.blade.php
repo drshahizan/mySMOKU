1e454a766e61c95eb6d0ea9d3445dbe0cc074a69
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-14 13:06:52 +0800

    Update semaksyarat.blade.php

--- resources/views/pages/auth/semaksyarat.blade.php
+++ resources/views/pages/auth/semaksyarat.blade.php
@@ -70,52 +70,142 @@
             <img alt="Logo" src="{{ image('logos/mqalogo.png') }}" class="h-100px h-lg-90px"/>
         </div>
 
-        <div class="fv-row mb-10">
-															
-            <label class="form-label required">Nama Pusat Pengajian</label>
-            <select name="id_institusi" class="form-select form-select-lg form-select-solid"  data-control="select2" data-placeholder="Pilih" data-allow-clear="true" data-hide-search="true">
+<!-- institusi Dropdown -->
+    <div class="fv-row mb-10">													
+        <label class="form-label required">Nama Pusat Pengajian</label>
+        <select id="id_institusi"  name="id_institusi" class="form-select form-select-lg form-select-solid js-example-basic-single"  data-control="select2" data-allow-clear="true" data-hide-search="true">
+            <option value='0'>Pilih</option>
             @foreach ($ipt as $ipt)	
-            <option></option> 
-            <option value="{{ $ipt->idipt}}">
-            {{ $ipt->namaipt}}
-            </option> 
+            <option value="{{ $ipt->idipt}}">{{ $ipt->namaipt}}</option> 
+            @endforeach
+        </select>
+    </div>
+
+    <div class="fv-row mb-10">
+            <label class="form-label required">Peringkat Pengajian</label>
+            <select id="peringkat_pengajian" name="peringkat_pengajian" class="form-select form-select-lg form-select-solid"  data-control="select2" data-allow-clear="true" data-hide-search="true">
+                <option>Pilih</option>
+                @foreach ($kodperingkat as $kodperingkat)	
+            <option value="{{ $kodperingkat->kodperingkat}}">{{ $kodperingkat->peringkat}}</option> 
             @endforeach
             </select>
-        
-        </div>
-        <!-- </div> -->
+    </div>
 
-        <div class="fv-row mb-10">
-            <label class="form-label required">Peringkat Pengajian</label>
-            <select name="peringkat_pengajian" class="form-select form-select-lg form-select-solid"  data-control="select2" data-placeholder="Pilih" data-allow-clear="true" data-hide-search="true">
-                @foreach ($peringkat as $peringkat)
-                @if ($peringkat->kodperingkat != 1 )
-                <option></option>
-                <option value="{{ $peringkat->kodperingkat}}">{{ $peringkat->peringkat}}</option>
-                @endif
-                @endforeach
-            </select>
-        </div>
-        <div class="fv-row mb-10">
 
-            <label class="form-label required">Nama Kursus</label>
-                    <select name="nama_kursus" class="form-select form-select-lg form-select-solid"  data-control="select2" data-placeholder="Pilih" data-allow-clear="true" data-hide-search="true">
-                        @foreach ($kursus as $kursus)	    
-                        <option></option>
-                        <option value="{{ $kursus->nama_kursus}}"> 
-                            {{ $kursus->nama_kursus}}
-                        </option>
-                        @endforeach
-                    </select>
-        </div>
+    <div class="fv-row mb-10">
+        <label class="form-label required">Nama Kursus</label>
+        <select id='nama_kursus' name='nama_kursus' class="form-select form-select-lg form-select-solid js-example-basic-single"  data-control="select2" data-allow-clear="true" data-hide-search="true">
+            <option value='0'>Pilih</option>
+        </select>
+    </div>
 
-       
+    <!-- Script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type='text/javascript'>
+    $(document).ready(function(){
+
+        // institusi Change
+        $('#id_institusi').change(function(){
+
+             // institusi id
+             var idipt = $(this).val();
+             var kodperingkat = $(this).val();
+
+             // Empty the dropdown
+             $('#peringkat_pengajian').find('option').not(':first').remove();
+             $('#nama_kursus').find('option').not(':first').remove();
+
+             // AJAX request 
+             $.ajax({
+                 url: 'getPeringkat/'+idipt,
+                 type: 'get',
+                 dataType: 'json',
+                 success: function(response){
+
+                     var len = 0;
+                     if(response['data'] != null){
+                          len = response['data'].length;
+                     }
+
+                     if(len > 0){
+                          // Read data and create <option >
+                          for(var i=0; i<len; i++){
+
+                               var idipt = response['data'][i].idipt;
+                               var kodperingkat = response['data'][i].kodperingkat;
+                               var peringkat = response['data'][i].peringkat;
+
+                               var option = "<option value='"+kodperingkat+"'>"+peringkat+"</option>";
+
+                               $("#peringkat_pengajian").append(option); 
+                          }
+                     }
+
+                 }
+             });
+
+        });
+
+        // peringkat Change
+        $('#peringkat_pengajian').change(function(){
+
+        // institusi id
+        var idipt = $(id_institusi).val();
+        var kodperingkat = $(this).val();
+
+        // Empty the dropdown
+       // $('#peringkat_pengajian').find('option').not(':first').remove();
+        $('#nama_kursus').find('option').not(':first').remove();
+        //alert(idipt);
+
+
+        // AJAX request 
+        $.ajax({
+            url: 'getKursus/'+kodperingkat+'/'+idipt,
+            type: 'get',
+            dataType: 'json',
+           
+            success: function(response){
+
+                var len = 0;
+                if(response['data'] != null){
+                    len = response['data'].length;
+                }
+
+                if(len > 0){
+                    // Read data and create <option >
+                    for(var i=0; i<len; i++){
+
+                        var idipt = response['data'][i].idipt;
+                        var kodperingkat = response['data'][i].kodperingkat;
+                        var nama_kursus = response['data'][i].nama_kursus;
+
+                        var option = "<option value='"+nama_kursus+"'>"+nama_kursus+"</option>";
+
+                        $("#nama_kursus").append(option); 
+                        
+                    }
+                }
+
+            }
+        });
+
+        });
+
+
+
+    });
+
+    $(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
+    </script>
 
 
         <!--begin::Submit button-->
         <div class="d-grid mb-10">
             <button type="submit"  class="btn btn-primary">
-                Seterusnya
+                Semak
             </button>
         </div>
         <!--end::Submit button-->
