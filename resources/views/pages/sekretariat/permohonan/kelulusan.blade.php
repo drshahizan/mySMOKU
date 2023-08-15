e9bdfaaaa8d6389929ebbe05ea8f1304c4ed8192
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-08-15 10:34:44 +0800

    update table kelulusan

--- resources/views/pages/sekretariat/permohonan/kelulusan.blade.php
+++ resources/views/pages/sekretariat/permohonan/kelulusan.blade.php
@@ -11,7 +11,6 @@
         <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
 
     <body>
