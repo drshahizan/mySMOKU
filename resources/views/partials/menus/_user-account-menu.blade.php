4410c99e288315f099e817ca41a940060d08c7ca
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-15 15:27:18 +0800

    Update _user-account-menu.blade.php

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -17,7 +17,7 @@
             <!--begin::Username-->
             <div class="d-flex flex-column">
                 <div class="fw-bold d-flex align-items-center fs-5">{{ Auth::user()->nokp}}
-                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
+                    <!-- <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span> -->
                 </div>
                 <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
             </div>
