78359ec2d202d9677fb2c3dcf2123311a28279ec
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-15 15:25:49 +0800

    Update _user-account-menu.blade.php

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -29,146 +29,63 @@
     <div class="separator my-2"></div>
     <!--end::Menu separator-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5">
-        <a href="#" class="menu-link px-5">My Profile</a>
-    </div>
+    <!-- <div class="menu-item px-5"> -->
+        <!-- <a href="#" class="menu-link px-5">My Profile</a> -->
+    <!-- </div> -->
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5">
-        <a href="#" class="menu-link px-5">
-            <span class="menu-text">My Projects</span>
-            <span class="menu-badge">
-                <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span>
-            </span>
-        </a>
-    </div>
+    <!-- <div class="menu-item px-5"> -->
+        <!-- <a href="#" class="menu-link px-5"> -->
+            <!-- <span class="menu-text">My Projects</span> -->
+            <!-- <span class="menu-badge"> -->
+                <!-- <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span> -->
+            <!-- </span> -->
+        <!-- </a> -->
+    <!-- </div> -->
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
+    <!-- <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0"> -->
         <!-- <a href="#" class="menu-link px-5">
             <span class="menu-title">My Subscription</span>
             <span class="menu-arrow"></span>
         </a> -->
         <!--begin::Menu sub-->
-        <div class="menu-sub menu-sub-dropdown w-175px py-4">
+        <!-- <div class="menu-sub menu-sub-dropdown w-175px py-4"> -->
             <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link px-5">Referrals</a>
-            </div>
+            <!-- <div class="menu-item px-3"> -->
+                <!-- <a href="#" class="menu-link px-5">Referrals</a> -->
+            <!-- </div> -->
             <!--end::Menu item-->
             <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link px-5">Billing</a>
-            </div>
+            <!-- <div class="menu-item px-3"> -->
+                <!-- <a href="#" class="menu-link px-5">Billing</a> -->
+            <!-- </div> -->
             <!--end::Menu item-->
             <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link px-5">Payments</a>
-            </div>
+            <!-- <div class="menu-item px-3"> -->
+                <!-- <a href="#" class="menu-link px-5">Payments</a> -->
+            <!-- </div> -->
             <!--end::Menu item-->
             <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex flex-stack px-5">Statements
-                    <span class="ms-2 lh-0" data-bs-toggle="tooltip" title="View your statements">{!! getIcon('information-5', 'fs-5') !!}</span></a>
-            </div>
+            <!-- <div class="menu-item px-3"> -->
+                <!-- <a href="#" class="menu-link d-flex flex-stack px-5">Statements -->
+                    <!-- <span class="ms-2 lh-0" data-bs-toggle="tooltip" title="View your statements">{!! getIcon('information-5', 'fs-5') !!}</span></a> -->
+            <!-- </div> -->
             <!--end::Menu item-->
             <!--begin::Menu separator-->
-            <div class="separator my-2"></div>
+            <!-- <div class="separator my-2"></div> -->
             <!--end::Menu separator-->
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <div class="menu-content px-3">
-                    <label class="form-check form-switch form-check-custom form-check-solid">
-						<input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-                        <span class="form-check-label text-muted fs-7">Notifications</span>
-                    </label>
-                </div>
-            </div>
+            
             <!--end::Menu item-->
-        </div>
+        <!-- </div> -->
         <!--end::Menu sub-->
-    </div>
+<!-- </div> -->
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5">
-        <a href="#" class="menu-link px-5">My Statements</a>
-    </div>
-    <!--end::Menu item-->
-    <!--begin::Menu separator-->
-    <div class="separator my-2"></div>
-    <!--end::Menu separator-->
-    <!--begin::Menu item-->
-    <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-        <a href="#" class="menu-link px-5">
-			<span class="menu-title position-relative">Mode 
-			<span class="ms-5 position-absolute translate-middle-y top-50 end-0">{!! getIcon('night-day', 'theme-light-show fs-2') !!} {!! getIcon('moon', 'theme-dark-show fs-2') !!}</span></span>
-		</a>
-		@include('partials/theme-mode/__menu')
-	</div>
-	<!--end::Menu item-->
-	<!--begin::Menu item-->
-	<div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-		<a href="#" class="menu-link px-5">
-            <span class="menu-title position-relative">Language
-                <span class="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">English
-                    <img class="w-15px h-15px rounded-1 ms-2" src="{{ image('flags/united-states.svg') }}" alt="" /></span>
-            </span>
-        </a>
-        <!--begin::Menu sub-->
-        <div class="menu-sub menu-sub-dropdown w-175px py-4">
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex px-5 active">
-                    <span class="symbol symbol-20px me-4">
-                        <img class="rounded-1" src="{{ image('flags/united-states.svg') }}" alt=""/>
-                    </span>
-                    English</a>
-            </div>
-            <!--end::Menu item-->
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex px-5">
-                    <span class="symbol symbol-20px me-4">
-                        <img class="rounded-1" src="{{ image('flags/spain.svg') }}" alt=""/>
-                    </span>
-                    Spanish</a>
-            </div>
-            <!--end::Menu item-->
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex px-5">
-                    <span class="symbol symbol-20px me-4">
-                        <img class="rounded-1" src="{{ image('flags/germany.svg') }}" alt=""/>
-                    </span>
-                    German</a>
-            </div>
-            <!--end::Menu item-->
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex px-5">
-                    <span class="symbol symbol-20px me-4">
-                        <img class="rounded-1" src="{{ image('flags/japan.svg') }}" alt=""/>
-                    </span>
-                    Japanese</a>
-            </div>
-            <!--end::Menu item-->
-            <!--begin::Menu item-->
-            <div class="menu-item px-3">
-                <a href="#" class="menu-link d-flex px-5">
-                    <span class="symbol symbol-20px me-4">
-                        <img class="rounded-1" src="{{ image('flags/france.svg') }}" alt=""/>
-                    </span>
-                    French</a>
-            </div>
-            <!--end::Menu item-->
-        </div>
-        <!--end::Menu sub-->
-    </div>
+
     <!--end::Menu item-->
     <!--begin::Menu item-->
-    <div class="menu-item px-5 my-1">
-        <a href="#" class="menu-link px-5">Account Settings</a>
-    </div>
+  
     <!--end::Menu item-->
     <!--begin::Menu item-->
     <div class="menu-item px-5">
