e2b3f44f50961a18e552b1ba771e9b643b3832c3
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-18 10:00:15 +0800

    Update dashboard.blade.php

--- resources/views/pages/penyelaras/dashboard.blade.php
+++ resources/views/pages/penyelaras/dashboard.blade.php
@@ -1,3 +1,392 @@
 <x-default-layout>
-    <h1>Selamat Datang, Penyelaras BKOKU IPT</h1>
+    	<!--begin::Page title-->
+<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+	<!--begin::Title-->
+	<h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Utama</h1>
+	<!--end::Title-->
+	<!--begin::Breadcrumb-->
+	<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+		<!--begin::Item-->
+		<li class="breadcrumb-item text-muted">
+			<a href="/" class="text-dark text-hover-primary" style="color:darkblue">Laman Utama</a>
+		</li>
+		<!--end::Item-->
+		<!--begin::Item-->
+		<li class="breadcrumb-item">
+			<span class="bullet bg-gray-400 w-5px h-2px"></span>
+		</li>
+		<!--end::Item-->
+		<!--begin::Item-->
+		<li class="breadcrumb-item text-dark" style="color:darkblue">Paparan Pemuka</li>
+		<!--end::Item-->
+	</ul>
+	<!--end::Breadcrumb-->
+</div>
+<head>
+    
+    <link rel="stylesheet" href="/assets/css/saringan.css">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script> -->
+    </head>
+<br>
+<!--begin::Row-->
+<!-- <div class="row g-5 g-xl-8">
+										<div class="col-xl-4">
+											
+											<a href="#" class="card bg-danger hoverable card-xl-stretch mb-xl-8">
+												
+												<div class="card-body">
+													<i class="ki-duotone ki-basket text-white fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+														<span class="path3"></span>
+														<span class="path4"></span>
+													</i>
+													<div class="text-white fw-bold fs-2 mb-2 mt-5">Shopping Cart</div>
+													<div class="fw-semibold text-white">Lands, Houses, Ranchos, Farms</div>
+												</div>
+												
+											</a>
+											
+										</div>
+										<div class="col-xl-4">
+											
+											<a href="#" class="card bg-primary hoverable card-xl-stretch mb-xl-8">
+												
+												<div class="card-body">
+													<i class="ki-duotone ki-cheque text-white fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+														<span class="path3"></span>
+														<span class="path4"></span>
+														<span class="path5"></span>
+														<span class="path6"></span>
+														<span class="path7"></span>
+													</i>
+													<div class="text-white fw-bold fs-2 mb-2 mt-5">Appartments</div>
+													<div class="fw-semibold text-white">Flats, Shared Rooms, Duplex</div>
+												</div>
+												
+											</a>
+										
+										</div>
+										<div class="col-xl-4">
+										
+											<a href="#" class="card bg-success hoverable card-xl-stretch mb-5 mb-xl-8">
+											
+												<div class="card-body">
+													<i class="ki-duotone ki-chart-simple-3 text-white fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+														<span class="path3"></span>
+														<span class="path4"></span>
+													</i>
+													<div class="text-white fw-bold fs-2 mb-2 mt-5">Sales Stats</div>
+													<div class="fw-semibold text-white">50% Increased for FY20</div>
+												</div>
+											
+											</a>
+											
+										</div>
+									</div> -->
+									<!--end::Row-->
+
+<!--begin::Row-->
+<div class="row g-5 g-xl-8">
+										<div class="col-xl-3">
+											<!--begin::Statistics Widget 5-->
+											<a href="#" class="card theme-dark-bg-body"  style="background-color: #4C6085">
+												<!--begin::Body-->
+												<div class="card-body">
+													<i class="ki-duotone ki-book text-primary fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+														<span class="path3"></span>
+														<span class="path4"></span>
+													</i>
+													<div class="fw-semibold text-white fs-4 mb-1 mt-1">Permohonan</div>
+													<div class="text-white fw-bold fs-2 mb-1 mt-1">500</div>
+													
+												</div>
+												<!--end::Body-->
+											</a>
+											<!--end::Statistics Widget 5-->
+										</div>
+										<div class="col-xl-3">
+											<!--begin::Statistics Widget 5-->
+											<a href="#" class="card theme-dark-bg-body"  style="background-color: #39A0ED">
+												<!--begin::Body-->
+												<div class="card-body">
+													<i class="ki-duotone ki-cheque text-gray-100 fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+														<span class="path3"></span>
+														<span class="path4"></span>
+														<span class="path5"></span>
+														<span class="path6"></span>
+														<span class="path7"></span>
+													</i>
+													<div class="fw-semibold text-white fs-4 mb-1 mt-1">Aktif</div>
+													<div class="text-white fw-bold fs-2 mb-1 mt-1">400</div>
+												</div>
+												<!--end::Body-->
+											</a>
+											<!--end::Statistics Widget 5-->
+										</div>
+										<div class="col-xl-3">
+											<!--begin::Statistics Widget 5-->
+											<a href="#" class="card theme-dark-bg-body"  style="background-color: #78cdd7">
+												<!--begin::Body-->
+												<div class="card-body">
+													<i class="ki-duotone ki-briefcase text-white fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+													</i>
+													<div class="fw-semibold text-white fs-4 mb-1 mt-1">Tidak Aktif</div>
+													<div class="text-white fw-bold fs-2 mb-1 mt-1">100</div>
+												</div>
+												<!--end::Body-->
+											</a>
+											<!--end::Statistics Widget 5-->
+										</div>
+										<div class="col-xl-3">
+											<!--begin::Statistics Widget 5-->
+											<a href="#" class="card bg-warning hoverable card-xl-stretch mb-5 mb-xl-8">
+												<!--begin::Body-->
+												<div class="card-body">
+													<i class="ki-duotone ki-chart-pie-simple text-white fs-2x ms-n1">
+														<span class="path1"></span>
+														<span class="path2"></span>
+													</i>
+													<div class="fw-semibold text-white fs-4 mb-1 mt-1">Dikembalikan</div>
+													<div class="text-white fw-bold fs-2 mb-1 mt-1">5 </div>
+												</div>
+												<!--end::Body-->
+											</a>
+											<!--end::Statistics Widget 5-->
+										</div>
+									</div>
+									<!--end::Row-->
+									<!--begin::Row-->
+									<div class="row g-5 g-xl-8">
+									<div class="col-xl-6">
+											<!--begin::Engage widget 9-->
+											<div class="card h-lg-100" style="background:  #ffffff )">
+												<!--begin::Body-->
+												<div class="card-body">
+													<!--begin::Row-->
+													<div class="row align-items-center">
+														
+														<!-- <div class="col-sm-7 pe-0 mb-5 mb-sm-0">
+															
+															<div class="d-flex justify-content-between h-100 flex-column pt-xl-5 pb-xl-2 ps-xl-7">
+															
+																<div class="mb-7">
+																	
+																	<div class="d-flex align-items-center flex-wrap d-grid gap-2"> -->
+																		
+																	<form class="form w-100" novalidate="novalidate" action="{{ route('register')}}" method="post"> 
+          @csrf 
+           <!--begin::Heading-->
+             <div class="text-center mb-11">
+            <!--begin::Title-->
+            <h1 class="text-dark fw-bolder mb-3">
+                Daftar Pelajar
+            </h1>
+            <!--end::Title-->
+            <br>
+            <br>
+            <!--begin::Subtitle-->
+            <h2 class="text-dark fw-bolder mb-3">
+                Semakan Sistem Maklumat Orang Kurang Upaya (SMOKU)
+            </h2>
+            <!--end::Subtitle--->
+             </div>
+         <!--begin::Heading-->
+
+            <div class="text-center mb-11">
+            <!--begin::Title-->
+            <!-- <h1 class="text-dark fw-bolder mb-3" > -->
+            <img alt="Logo" src="{{ image('logos/logo.png') }}" class="h-100px h-lg-90px"/>        
+            </div>
+
+            <!--begin::Input group-"-->
+            <div class="col-xs-3">
+            <!--begin::Name-->
+            <input type="text" placeholder="No Kad Pengenalan" name="nokp" maxlength="12" autocomplete="off" class="form-control bg-transparent"  value=""/>
+            <!--end::Name-->
+            </div>
+
+        <!--begin::Submit button-->
+            <div class="d-flex flex-center mt-15">
+            <button type="submit"  class="btn btn-primary">
+                Daftar
+            </button>
+             </div>
+            </form>
+																	</div>
+									
+																</div>
+																
+																
+															</div>
+														
+ </div>
+ <!--begin::Row-->
+ 
+										<!--begin::Col-->
+										<div class="col-xl-6">
+											<!--begin::Table widget 9-->
+											<div class="card card-flush h-xl-100">
+												<!--begin::Header-->
+												<div class="card-header pt-5">
+													<!--begin::Title-->
+													<h3 class="card-title align-items-start flex-column">
+														<span class="card-label fw-bold text-gray-800">Pendaftaran Pelajar</span>
+														<span class="text-gray-400 pt-1 fw-semibold fs-6">Program BKOKU</span>
+													</h3>
+													<!--end::Title-->
+													<!--begin::Toolbar-->
+													<!-- <div class="card-toolbar">
+														<a href="#" class="btn btn-sm btn-light">PDF Report</a>
+													</div> -->
+													<!--end::Toolbar-->
+												</div>
+												<!--end::Header-->
+												<!--begin::Body-->
+												<div class="card-body py-3">
+													<!--begin::Table container-->
+													<div class="table-responsive">
+														<!--begin::Table-->
+														<table id="sortTable" class="table table-striped table-hover dataTable js-exportable">
+															<!--begin::Table head-->
+															<thead>
+																<tr class="fs-7 fw-bold border-0 text-dark">
+																	<th class="min-w-150px text-center" colspan="2">Nama</th>
+																	<th class="min-w-150px text-center" colspan="2">No Matrik</th>
+																	<th class="min-w-150px text-center" colspan="2">ID Permohonan</th>
+																	<th class="text-center min-w-150px" colspan="2">Status Permohonan</th>
+																</tr>
+															</thead>
+															<!--end::Table head-->
+															<!--begin::Table body-->
+															<tbody>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Mohammad Nik Zariq bin Syed Samiun Hammani</span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A24CS0001</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/1/140127234321</span>
+																		</div>
+																	</td>
+																	
+																	<td class="text-center"><button class="btn bg-primary text-white">Deraf</button></td>
+																	
+																</tr>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Zahrah binti Syed Sualman Hakiem</span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A22CS0231</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/1/130127223322</span>
+																		</div>
+																	</td>
+																	<td class="text-center"><button class="btn bg-primary text-white">Deraf</button></td>
+																</tr>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Cheok Thur Qi </span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A22MM3001</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/2/110127234322</span>
+																		</div>
+																	</td>
+																	<td class="text-center"><button class="btn bg-primary text-white">Deraf</button></td>
+																</tr>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Mohammad Nik Zariq bin Syed Samiun Hammani</span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A24CS0001</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/1/140127234321</span>
+																		</div>
+																	</td>
+																	<td class="text-center"><button class="btn bg-primary text-white">Deraf</button></td>
+																</tr>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Mohammad Nik Zariq bin Syed Samiun Hammani</span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A24CS0001</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/1/140127234321</span>
+																		</div>
+																	</td>
+																	<td class="text-center"><button class="btn bg-primary text-white">Deraf</button></td>
+																</tr>
+																<tr>
+																	<td class="" colspan="2">
+																		<span class="text-gray-800 fw-bold text-center mb-1 fs-6">Mohammad Nik Zariq bin Syed Samiun Hammani</span>
+																	</td>
+																	<td class="pe-0" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-gray-800 fw-bold fs-6 me-1">A24CS0001</span>
+																		</div>
+																	</td>
+																	<td class="" colspan="2">
+																		<div class="d-flex justify-content-end">
+																			<span class="text-dark fw-bold fs-6 me-3">KPTBKOKU/1/140127234321</span>
+																		</div>
+																	</td>
+																	<td class="text-center"><button class="btn bg-orange text-white"> Belum Disaring</button></td>
+																</tr>
+															</tbody>
+															<!--end::Table body-->
+														</table>
+														<!--end::Table-->
+													</div>
+													<!--end::Table container-->
+												</div>
+												<!--end::Body-->
+											</div>
+											<!--end::Table Widget 9-->
+</div>
+</div> <script>
+        $('#sortTable').DataTable();
+    </script>
+</div>
+
+
+
+
 </x-default-layout>
