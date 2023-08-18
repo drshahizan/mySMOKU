93d2f9b2940559c33a576d8268ca44dc92c295e8
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-08-18 20:33:08 +0800

    Update _menu.blade.php

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -95,7 +95,7 @@
 				</div>
 			</div>
 			<div class="menu-item">
-			<a class="menu-link" href="{{ route('keseluruhanpermohonan')}}">
+			<a class="menu-link" href="">
 					<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
 					<span class="menu-title">Keseluruhan</span>
 			</a>
