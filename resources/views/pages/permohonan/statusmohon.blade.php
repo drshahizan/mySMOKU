e19ebbc3b69280858ab1ffccef97ad841d7456d1
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-21 16:20:05 +0800

    Update statusmohon.blade.php

--- resources/views/pages/permohonan/statusmohon.blade.php
+++ resources/views/pages/permohonan/statusmohon.blade.php
@@ -1,4 +1,12 @@
 <x-default-layout> 
+<head>
+   
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="/assets/css/saringan.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+    </head>
 		<!--begin::Page title-->
 <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
 	<!--begin::Title-->
