e500c58845689174d6a18000b64669a4cb402e98
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-08-22 17:17:41 +0800

    Perbaharui Permohonan lagi

--- resources/views/pages/permohonan/baharuimohon.blade.php
+++ resources/views/pages/permohonan/baharuimohon.blade.php
@@ -87,86 +87,14 @@
 																
 															</div>
 														
-															<!-- <div class="table-responsive mb-10">
-																
-																<table class="table g-5 gs-0 mb-0 fw-bold text-gray-700" data-kt-element="items">
-																	
-																	<thead>
-																		<tr class="border-bottom fs-6 fw-bold text-gray-700 mb-3">
-																			<th class="min-w-300px w-100px">Salinan Keputusan Pengajian</th>
-																			<th class="min-w-100px w-150px">GPA</th>
-																			<th class="min-w-100px w-150px">CGPA</th>
-																			<th class="min-w-100px w-150px">Tindakan</th>
-																		</tr>
-																	</thead>
-																	
-																	<tbody>
-																		<tr class="border-bottom border-bottom-dashed" data-kt-element="item">
-																			<td class="pe-7">
-																				<input type="text" class="form-control form-control-solid mb-2" name="name[]" placeholder="Item name" />
-																			
-																			</td>
-																			<td class="ps-0">
-																				<input class="form-control form-control-solid" type="number" min="1" name="quantity[]" placeholder="1" value="1" data-kt-element="quantity" />
-																			</td>
-																			<td>
-																				<input type="text" class="form-control form-control-solid text-end" name="price[]" placeholder="0.00" value="0.00" data-kt-element="price" />
-																			</td>
-																			<td class="pt-5 text-end">
-																				<button type="button" class="btn btn-sm btn-icon btn-active-color-primary" data-kt-element="remove-item">
-																					<i class="ki-duotone ki-trash fs-3">
-																						<span class="path1"></span>
-																						<span class="path2"></span>
-																						<span class="path3"></span>
-																						<span class="path4"></span>
-																						<span class="path5"></span>
-																					</i>
-																				</button>
-																			</td>
-																		</tr>
-																	</tbody>
-																</table>
-															</div>
-															 -->
-															<!-- <table class="table d-none" data-kt-element="item-template">
-																<tr class="border-bottom border-bottom-dashed" data-kt-element="item">
-																	<td class="pe-7">
-																		<input type="text" class="form-control form-control-solid mb-2" name="name[]" placeholder="Item name" />
-																		<input type="text" class="form-control form-control-solid" name="description[]" placeholder="Description" />
-																	</td>
-																	<td class="ps-0">
-																		<input class="form-control form-control-solid" type="number" min="1" name="quantity[]" placeholder="1" data-kt-element="quantity" />
-																	</td>
-																	<td>
-																		<input type="text" class="form-control form-control-solid text-end" name="price[]" placeholder="0.00" data-kt-element="price" />
-																	</td>
-																	<td class="pt-8 text-end">$
-																	<span data-kt-element="total">0.00</span></td>
-																	<td class="pt-5 text-end">
-																		<button type="button" class="btn btn-sm btn-icon btn-active-color-primary" data-kt-element="remove-item">
-																			<i class="ki-duotone ki-trash fs-3">
-																				<span class="path1"></span>
-																				<span class="path2"></span>
-																				<span class="path3"></span>
-																				<span class="path4"></span>
-																				<span class="path5"></span>
-																			</i>
-																		</button>
-																	</td>
-																</tr>
-															</table> -->
-															<table class="table d-none" data-kt-element="empty-template">
+															
+															<!-- <table class="table d-none" data-kt-element="empty-template">
 																<tr data-kt-element="empty">
 																	<th colspan="5" class="text-muted text-center py-10">No items</th>
 																</tr>
-															</table>
-															<!--end::Item template-->
-															<!--begin::Notes-->
-															<!-- <div class="mb-0">
-																<label class="form-label fs-6 fw-bold text-gray-700">Notes</label>
-																<textarea name="notes" class="form-control form-control-solid" rows="3" placeholder="Thanks for your business"></textarea>
-															</div> -->
-															<!--end::Notes-->
+															</table> -->
+															<a href="#" class="btn btn-success hover-rotate-end">Hantar</a>
+															<a href="#" class="btn btn-danger hover-scale">Batal</a>
 														</div>
 														<!--end::Wrapper-->
 													</form>
