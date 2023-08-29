3a0f6b8ec2e2ec7f9deac18ff0712e3ab7aee836
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-08-29 09:45:00 +0800

    Update _menu.blade.php

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -233,46 +233,28 @@
 				<!--begin::Menu-->
 				<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
 				<!--begin:Menu item-->
-				<div class="menu-item">
-						<a class="menu-link" href="{{ route('dashboard') }}">
-							<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
-							<span class="menu-title">Laman Utama</span>
-						</a>
-				</div>	
-				<div class="menu-item pt-5">
-					<div class="menu-content">
-						<span class="menu-heading fw-bold text-uppercase fs-7">Pentadbir Sistem</span>
+					<div class="menu-item">
+							<a class="menu-link" href="{{ route('dashboard') }}">
+								<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
+								<span class="menu-title">Laman Utama</span>
+							</a>
+					</div>	
+					<div class="menu-item pt-5">
+						<div class="menu-content">
+							<span class="menu-heading fw-bold text-uppercase fs-7">Pentadbir Sistem</span>
+						</div>
 					</div>
-				</div>
+					
+					<div class="menu-item">
+						<a class="menu-link" href="{{url('daftarpengguna')}}">
+								<span class="menu-icon">{!! getIcon('notepad-edit', 'fs-2') !!}</span>
+								<span class="menu-title">Daftar Pengguna</span>
+						</a>
+					</div>
 				
-				<div class="menu-item">
-					<a class="menu-link" href="{{url('daftarpengguna')}}">
-							<span class="menu-icon">{!! getIcon('notepad-edit', 'fs-2') !!}</span>
-							<span class="menu-title">Daftar Pengguna</span>
-					</a>
 				</div>
-				
-			</div>
-		</div>
-			<!--end::Menu-->		
-
+			<!--end::Menu-->
 @endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		
 	</div>
 	<!--end::Menu wrapper-->
