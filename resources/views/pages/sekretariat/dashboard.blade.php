f311977652faaa9877c4a28e05bc68c88683cc0b
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-09-03 16:24:58 +0800

    update

--- resources/views/pages/sekretariat/dashboard.blade.php
+++ resources/views/pages/sekretariat/dashboard.blade.php
@@ -16,12 +16,12 @@
 	<!--begin::Page title-->
 	<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
 		<!--begin::Title-->
-		<h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Utama</h1>
+		<h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Laman Utama</h1>
 		<!--end::Title-->
 		<!--begin::Breadcrumb-->
 		<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
 			<!--begin::Item-->
-			<li class="breadcrumb-item text-dark" style="color:darkblue">Utama</li>
+			<li class="breadcrumb-item text-dark" style="color:darkblue">Laman Utama</li>
 			<!--end::Item-->
 			<!--begin::Item-->
 			<li class="breadcrumb-item">
