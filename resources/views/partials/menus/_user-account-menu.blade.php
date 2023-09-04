10265c542a327f57bdcdca7349b056d90d963711
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-09-04 11:49:01 +0800

    update

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -12,7 +12,6 @@
                     <img alt="Logo" src="{{ Auth::user()->profile_photo_path }}"/>
                 @else
                     <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle('bg-light-? text-?', $nama) }}">
-                    <!-- <div class="symbol-label fs-3"> -->
                         {{ substr($nama,0,1) }}
                     </div>
                 @endif
