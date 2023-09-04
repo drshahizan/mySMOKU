fa45730c00d10fe40be9398751830c5825c28f8f
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-09-04 13:10:47 +0800

    push

--- resources/views/pages/sekretariat/dashboard.blade.php
+++ resources/views/pages/sekretariat/dashboard.blade.php
@@ -1,10 +1,6 @@
 <x-default-layout>
 	<head>
-		<title>Sekretariat BKOKU KPT</title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-
+		
         <!-- MAIN CSS -->
         <link rel="stylesheet" href="/assets/css/sekretariat.css">
 		<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
