93989c98bf98a35780a0773fda057856a522ff1b
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-09-06 00:07:20 +0800

    Update baharuimohon.blade.php

--- resources/views/pages/permohonan/baharuimohon.blade.php
+++ resources/views/pages/permohonan/baharuimohon.blade.php
@@ -58,6 +58,7 @@
 																			<option></option>
 																			<option value="1">1</option>
 																			<option value="2">2</option>					
+																			<option value="3">3</option>					
 																		</select>
 																	</div>
 																</div>
