2f8e76cabdccb0e264c9f7c321d6ccc82d287bf2
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-09-14 15:46:09 +0800

    Update register.blade.php

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -32,7 +32,7 @@
         <!--begin::Input group--->
         <div class="fv-row mb-8">
             <!--begin::Name-->
-            <input type="text" placeholder="No Kad Pengenalan atau No. JKM" name="nokp" maxlength="12" autocomplete="off" class="form-control bg-transparent"  value=""/>
+            <input type="text" placeholder="No Kad Pengenalan" name="no_kp" maxlength="12" autocomplete="off" class="form-control bg-transparent"  value=""/>
             <!--end::Name-->
         </div>
 
