fe2806da87f7805c00c17fff5031c6ed5e31190e
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-09-17 15:07:47 +0800

    update sidebar pelajar

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -5,76 +5,87 @@
 	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
 		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-		<!--begin:Menu item-->
-		<div class="menu-item">
+			<!--begin:Menu item-->
+			<div class="menu-item">
 				<a class="menu-link" href="{{ route('dashboard') }}">
 					<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
 					<span class="menu-title">Laman Utama</span>
 				</a>
-		</div>
+			</div>
+			<div class="menu-item pt-5">
+				<div class="menu-content">
+					<span class="menu-heading fw-bold text-uppercase fs-7">Pengajian</span>
+				</div>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="{{ route('baharuimohon') }}">
+					<span class="menu-icon">{!! getIcon('update-file', 'fs-2') !!}</span>
+					<span class="menu-title">Kemaskini Keputusan Peperiksaan</span>
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="{{ route('baharuimohon') }}">
+					<span class="menu-icon">{!! getIcon('update-file', 'fs-2') !!}</span>
+					<span class="menu-title">Lapor Diri Tamat Pengajian</span>
+				</a>
+			</div>
+
 			<div class="menu-item pt-5">
 				<div class="menu-content">
 					<span class="menu-heading fw-bold text-uppercase fs-7">Permohonan</span>
 				</div>
 			</div>
 			<div class="menu-item">
-			<a class="menu-link" href="permohonan">
-			<!-- "{{ route('dashboard') }}" -->
+				<a class="menu-link" href="permohonan">
 					<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
 					<span class="menu-title">Baru</span>
-			</a>
-		</div>
-
-		<div class="menu-item">
-			<a class="menu-link" href="{{ route('permohonan.sejarah') }}">
-
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="{{ route('permohonan.sejarah') }}">
 					<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
 					<span class="menu-title">Sejarah</span>
-			</a>
-		</div>
-
-		<div class="menu-item">
-			<a class="menu-link" href="{{ route('baharuimohon') }}">
-
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="{{ route('baharuimohon') }}">
 					<span class="menu-icon">{!! getIcon('update-file', 'fs-2') !!}</span>
 					<span class="menu-title">Perbaharui</span>
-			</a>
-		</div>
+				</a>
+			</div>
 
-		<div class="menu-item pt-5">
+			<div class="menu-item pt-5">
 				<div class="menu-content">
 					<span class="menu-heading fw-bold text-uppercase fs-7">Tuntutan</span>
 				</div>
 			</div>
 			<div class="menu-item">
-			<a class="menu-link" href="{{ route('borangTuntutanYuran') }}">
-			<!-- "{{ route('dashboard') }}" -->
+				<a class="menu-link" href="{{ route('borangTuntutanYuran') }}">
 					<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
 					<span class="menu-title">Baru</span>
-			</a>
-		</div>
-		<div class="menu-item">
-			<a class="menu-link" href="#">
-			<!-- "{{ route('dashboard') }}" -->
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="#">
 					<span class="menu-icon">{!! getIcon('notepad-edit', 'fs-2') !!}</span>
 					<span class="menu-title">Kemaskini</span>
-			</a>
-		</div>
-		<div class="menu-item">
-			<a class="menu-link" href="#">
-			<!-- "{{ route('dashboard') }}" -->
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="#">
 					<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
 					<span class="menu-title">Sejarah</span>
-			</a>
-		</div>
-		<div class="menu-item">
-			<a class="menu-link" href="#">
+				</a>
+			</div>
+			<div class="menu-item">
+				<a class="menu-link" href="#">
 					<span class="menu-icon">{!! getIcon('status', 'fs-2') !!}</span>
 					<span class="menu-title">Status</span>
-			</a>
-		</div>
+				</a>
+			</div>
 		</div>
 		<!--end::Menu-->
+
 @elseif(Auth::user()->tahap=='2')
 <!--begin::sidebar menu-->
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
@@ -152,7 +163,6 @@
 		<!--end::Menu-->
 
 @elseif(Auth::user()->tahap=='3')
-
 <!--begin::sidebar menu-->
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
 	<!--begin::Menu wrapper-->
