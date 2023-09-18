d6960d04af5bfc86705a1e56ca8eb9ec1e534c6c
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-09-19 07:51:43 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -83,20 +83,20 @@ class DashboardController extends Controller
             return view('pages.pentadbir.dashboard')->with('message', 'Selamat Datang ke Laman Utama Pegawai Atasan');
         }
         
-        else if(Auth::user()->tahap=='6'){
-            $smoku = Smoku::orderBy('smoku.id','desc')
-                ->leftJoin('permohonan','permohonan.nokp_pelajar','=','smoku.nokp')
-                ->get(['smoku.*', 'permohonan.*'])
-                ->where('status','!=', '2')
-                ->where('jenis','=', 'PPK');
-                // ->where('jenis_penyelaras','=', 2);
-            return view('pages.penyelarasppk.dashboard', compact('smoku'));
-        }
+        // else if(Auth::user()->tahap=='6'){
+        //     $smoku = Smoku::orderBy('smoku.id','desc')
+        //         ->leftJoin('permohonan','permohonan.nokp_pelajar','=','smoku.nokp')
+        //         ->get(['smoku.*', 'permohonan.*'])
+        //         ->where('status','!=', '2')
+        //         ->where('jenis','=', 'PPK');
+        //         // ->where('jenis_penyelaras','=', 2);
+        //     return view('pages.penyelarasppk.dashboard', compact('smoku'));
+        // }
         else{
             return view('page.auth.login')->with('message', 'Unauthorized');
         }
-       
     }
+    
 
     public function store(Request $request)
     {  
@@ -120,3 +120,7 @@ class DashboardController extends Controller
         return back()->with('success', 'Avatar updated successfully.');
     }
 }
+    
+
+
+    
