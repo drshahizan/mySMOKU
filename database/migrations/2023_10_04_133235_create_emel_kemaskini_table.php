dcfe82ba348d344f69124c52baa21cb9a67f1daf
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-10-04 13:45:44 +0800

    emel database

---
+++ database/migrations/2023_10_04_133235_create_emel_kemaskini_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emel_kemaskini', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('emel_id');
+            $table->string('subjek')->nullable();
+            $table->string('pendahuluan')->nullable();
+            $table->string('isi_kandungan')->nullable();
+            $table->string('penutup')->nullable();
+            $table->string('disediakan_oleh')->nullable();
+            $table->foreign('emel_id')->references('id')->on('bk_emel')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emel_kemaskini');
+    }
+};
