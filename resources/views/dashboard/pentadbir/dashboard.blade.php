c445a315c14299ab66379d000a94cbd9ff750ce0
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-10-12 02:47:15 +0800

    Test dashboard admin

--- resources/views/dashboard/pentadbir/dashboard.blade.php
+++ resources/views/dashboard/pentadbir/dashboard.blade.php
@@ -1,3 +1,55 @@
 <x-default-layout>
     <h1>Selamat Datang, Admin</h1>
+    <!--begin::Row-->
+    <div class="row g-5 g-xl-8">
+        <div class="col-xl-4">
+            <!--begin: Statistics Widget 6-->
+            <div class="card bg-light-success card-xl-stretch mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body my-3">
+                    <a href="#" class="card-title fw-bold text-success fs-5 mb-3 d-block">Project Progress</a>
+                    <div class="py-1">
+                        <span class="text-dark fs-1 fw-bold me-2">50%</span>
+                        <span class="fw-semibold text-muted fs-7">Avarage</span>
+                    </div>
+
+                </div>
+                <!--end:: Body-->
+            </div>
+            <!--end: Statistics Widget 6-->
+        </div>
+        <div class="col-xl-4">
+            <!--begin: Statistics Widget 6-->
+            <div class="card bg-light-warning card-xl-stretch mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body my-3">
+                    <a href="#" class="card-title fw-bold text-warning fs-5 mb-3 d-block">Company Finance</a>
+                    <div class="py-1">
+                        <span class="text-dark fs-1 fw-bold me-2">15%</span>
+                        <span class="fw-semibold text-muted fs-7">48k Goal</span>
+                    </div>
+
+                </div>
+                <!--end:: Body-->
+            </div>
+            <!--end: Statistics Widget 6-->
+        </div>
+        <div class="col-xl-4">
+            <!--begin: Statistics Widget 6-->
+            <div class="card bg-light-primary card-xl-stretch mb-5 mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body my-3">
+                    <a href="#" class="card-title fw-bold text-primary fs-5 mb-3 d-block">Marketing Analysis</a>
+                    <div class="py-1">
+                        <span class="text-dark fs-1 fw-bold me-2">76%</span>
+                        <span class="fw-semibold text-muted fs-7">400k Impressions</span>
+                    </div>
+
+                </div>
+                <!--end:: Body-->
+            </div>
+            <!--end: Statistics Widget 6-->
+        </div>
+    </div>
+    <!--end::Row-->
 </x-default-layout>
