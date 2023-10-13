99b8ed6c1f291ebe747226921c9ee55847a092de
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-10-13 12:59:44 +0800

    Masih betulkan card kat bahagian dashboard admin, Jangan kacau eh

--- resources/views/dashboard/pentadbir/dashboard.blade.php
+++ resources/views/dashboard/pentadbir/dashboard.blade.php
@@ -1,24 +1,24 @@
 <x-default-layout>
     <h1>Selamat Datang, Admin</h1>
+    <br>
     <!--begin::Row-->
-    <div class="row g-5 g-xl-8">
-        <div class="col-xl-4">
+    <div class="row">
+        <div class="col-md-4 col-xl-3">
             <!--begin: Statistics Widget 6-->
             <div class="card bg-light-success card-xl-stretch mb-xl-8">
                 <!--begin::Body-->
                 <div class="card-body my-3">
-                    <a href="#" class="card-title fw-bold text-success fs-5 mb-3 d-block">Project Progress</a>
+                    <a href="#" class="card-title fw-bold text-success fs-5">Keseluruhan Pengguna</a>
                     <div class="py-1">
-                        <span class="text-dark fs-1 fw-bold me-2">50%</span>
-                        <span class="fw-semibold text-muted fs-7">Avarage</span>
-                    </div>
-
+                        <i class="bi bi-person-circle text-success fs-3x"></i>
+                            <span class="text-dark fs-1 fw-bold m-3">15%</span>
+                     </div>
                 </div>
                 <!--end:: Body-->
             </div>
             <!--end: Statistics Widget 6-->
         </div>
-        <div class="col-xl-4">
+        <div class="col-xl-3">
             <!--begin: Statistics Widget 6-->
             <div class="card bg-light-warning card-xl-stretch mb-xl-8">
                 <!--begin::Body-->
@@ -34,7 +34,7 @@
             </div>
             <!--end: Statistics Widget 6-->
         </div>
-        <div class="col-xl-4">
+        <div class="col-xl-3">
             <!--begin: Statistics Widget 6-->
             <div class="card bg-light-primary card-xl-stretch mb-5 mb-xl-8">
                 <!--begin::Body-->
@@ -50,6 +50,20 @@
             </div>
             <!--end: Statistics Widget 6-->
         </div>
+        <div class="col-xl-3">
+            <!--begin: Statistics Widget 6-->
+            <div class="card bg-light-primary card-xl-stretch mb-5 mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body my-3">
+                    <a href="#" class="card-title fw-bold text-primary fs-5 mb-3 d-block">Marketing Analysis</a>
+                    <div class="py-1">
+                        <span class="text-dark fs-1 fw-bold me-2">76%</span>
+                        <span class="fw-semibold text-muted fs-7">400k Impressions</span>
+                    </div>
+
+                </div>
+                <!--end:: Body-->
+            </div>
     </div>
     <!--end::Row-->
 </x-default-layout>
