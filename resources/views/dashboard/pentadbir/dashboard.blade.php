5c6683ba0c6e7cfb6c6210c93d5837db3b595192
Adlina9727 <adlinaabdulsamad@gmail.com>
2023-10-13 13:48:14 +0800

    Test dulu admin dashboard

--- resources/views/dashboard/pentadbir/dashboard.blade.php
+++ resources/views/dashboard/pentadbir/dashboard.blade.php
@@ -3,15 +3,15 @@
     <br>
     <!--begin::Row-->
     <div class="row">
-        <div class="col-md-4 col-xl-3">
+        <div class="col-sm-6 col-xl-2 mb-xl-10">
             <!--begin: Statistics Widget 6-->
-            <div class="card bg-light-success card-xl-stretch mb-xl-8">
+            <div class="card bg-light-success h-lg-100">
                 <!--begin::Body-->
                 <div class="card-body my-3">
-                    <a href="#" class="card-title fw-bold text-success fs-5">Keseluruhan Pengguna</a>
+                    <a href="#" class="card-title fw-bold text-success fs-6">Jumlah Pengguna</a>
                     <div class="py-1">
                         <i class="bi bi-person-circle text-success fs-3x"></i>
-                            <span class="text-dark fs-1 fw-bold m-3">15%</span>
+                            <span class="text-dark fs-3x  fw-bold me-2">15%</span>
                      </div>
                 </div>
                 <!--end:: Body-->
@@ -66,4 +66,242 @@
             </div>
     </div>
     <!--end::Row-->
+
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-chart-simple fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                        <span class="path3"></span>
+                        <span class="path4"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">27,5M</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">Stock Qty</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-success fs-base">
+                <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>2.1%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-abstract-39 fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">149M</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">Stock Value</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-danger fs-base">
+                <i class="ki-duotone ki-arrow-down fs-5 text-danger ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>0.47%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-map fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                        <span class="path3"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">89M</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">C APEX</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-success fs-base">
+                <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>2.1%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-5 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-abstract-35 fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">72.4%</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">OPEX</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-danger fs-base">
+                <i class="ki-duotone ki-arrow-down fs-5 text-danger ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>0.647%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-5 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-abstract-26 fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">106M</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">Saving</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-success fs-base">
+                <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>2.1%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
+    <!--begin::Col-->
+    <div class="col-sm-6 col-xl-2 mb-5 mb-xl-10">
+        <!--begin::Card widget 2-->
+        <div class="card h-lg-100">
+            <!--begin::Body-->
+            <div class="card-body d-flex justify-content-between align-items-start flex-column">
+                <!--begin::Icon-->
+                <div class="m-0">
+                    <i class="ki-duotone ki-abstract-26 fs-2hx text-gray-600">
+                        <span class="path1"></span>
+                        <span class="path2"></span>
+                    </i>
+                </div>
+                <!--end::Icon-->
+                <!--begin::Section-->
+                <div class="d-flex flex-column my-7">
+                    <!--begin::Number-->
+                    <span class="fw-semibold fs-3x text-gray-800 lh-1 ls-n2">106M</span>
+                    <!--end::Number-->
+                    <!--begin::Follower-->
+                    <div class="m-0">
+                        <span class="fw-semibold fs-6 text-gray-400">Saving</span>
+                    </div>
+                    <!--end::Follower-->
+                </div>
+                <!--end::Section-->
+                <!--begin::Badge-->
+                <span class="badge badge-light-success fs-base">
+                <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                </i>2.1%</span>
+                <!--end::Badge-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Card widget 2-->
+    </div>
+    <!--end::Col-->
 </x-default-layout>
