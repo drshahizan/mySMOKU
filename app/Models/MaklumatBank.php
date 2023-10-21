9b2b98a8ac34d18919f161a16f1c86bf538bf2e0
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-10-21 23:25:33 +0800

    MAKLUMAT BANK IPTA

---
+++ app/Models/MaklumatBank.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaklumatBank extends Model
+{
+    use HasFactory;
+    protected $table = 'maklumat_bank';
+
+    protected $fillable = [
+        'institusi_id',
+        'nama_akaun',
+        'no_akaun',
+    ];
+}
