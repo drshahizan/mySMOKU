0929a75a25495fc03584fc0e5ece5638d9819a9f
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-10-24 13:36:10 +0800

    Update MaklumatKursusController.php

--- app/Http/Controllers/MaklumatKursusController.php
+++ app/Http/Controllers/MaklumatKursusController.php
@@ -11,8 +11,6 @@ use Illuminate\Support\Facades\Http;
 
 class MaklumatKursusController extends Controller
 {
-
-
     public function index()
     {
         $response = Http::post('http://10.29.216.151/api/bkoku/request-MQR');
@@ -20,19 +18,24 @@ class MaklumatKursusController extends Controller
         if ($response->successful()) {
             $data = json_decode($response->body(), true);
 
-            if (isset($data['dataMQR'])) {
-                foreach ($data['dataMQR'] as $item) {
-                    MaklumatKursusMQA::create($item); // Assuming $item is an associative array containing your data
+            // if (isset($data['dataMQR'])) {
+            //     foreach ($data['dataMQR'] as $item) {
+            //         MaklumatKursusMQA::create($item); // Assuming $item is an associative array containing your data
+            //     }
+            //     return response()->json(['message' => 'Data inserted successfully'], 200);
+            // } else {
+            //     return response()->json(['error' => 'Invalid API response format'], 500);
+            // }
+            foreach ($data['dataMQR'] as $item) {
+                foreach ($item as $key => $value) {
+                    echo $key . ": " . $value . "<br>";
+                    
                 }
-                return response()->json(['message' => 'Data inserted successfully'], 200);
-            } else {
-                return response()->json(['error' => 'Invalid API response format'], 500);
+                echo "<br>"; // Add a line break between items
             }
         } else {
             return response()->json(['error' => 'Unable to fetch data from API'], $response->status());
         }
     }
-
-
 
 }
