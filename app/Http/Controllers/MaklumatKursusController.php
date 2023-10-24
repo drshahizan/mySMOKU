f556ac621e5cf4897b928112d831069125221836
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-10-24 13:45:29 +0800

    ok

--- app/Http/Controllers/MaklumatKursusController.php
+++ app/Http/Controllers/MaklumatKursusController.php
@@ -18,21 +18,28 @@ class MaklumatKursusController extends Controller
         if ($response->successful()) {
             $data = json_decode($response->body(), true);
 
-            // if (isset($data['dataMQR'])) {
-            //     foreach ($data['dataMQR'] as $item) {
-            //         MaklumatKursusMQA::create($item); // Assuming $item is an associative array containing your data
-            //     }
-            //     return response()->json(['message' => 'Data inserted successfully'], 200);
-            // } else {
-            //     return response()->json(['error' => 'Invalid API response format'], 500);
-            // }
-            foreach ($data['dataMQR'] as $item) {
-                foreach ($item as $key => $value) {
-                    echo $key . ": " . $value . "<br>";
-                    
+            if (isset($data['dataMQR'])) {
+                $counter = 0;
+                foreach ($data['dataMQR'] as $item) {
+                    if ($counter < 10) {
+                        MaklumatKursusMQA::create($item);
+                        $counter++;
+                    } else {
+                        break; // Break the loop after inserting 10 records
+                    }
                 }
-                echo "<br>"; // Add a line break between items
+                return response()->json(['message' => 'Data inserted successfully'], 200);
+            } else {
+                return response()->json(['error' => 'Invalid API response format'], 500);
             }
+            //untuk papar
+            // foreach ($data['dataMQR'] as $item) {
+            //     foreach ($item as $key => $value) {
+            //         echo $key . ": " . $value . "<br>";
+                    
+            //     }
+            //     echo "<br>"; // Add a line break between items
+            // }
         } else {
             return response()->json(['error' => 'Unable to fetch data from API'], $response->status());
         }
--- app/Models/MaklumatKursusMQA.php
+++ app/Models/MaklumatKursusMQA.php
@@ -11,18 +11,18 @@ class MaklumatKursusMQA extends Model
     protected $table = 'bk_kursus_mqa';
 
     protected $fillable = [
-        'kod_rujukan_akreditasi_penuh',
-        'no_siri_sijil',
-        'nama_program_bm',
-        'nama_program_bi',
-        'kod_nec',
-        'bidang_program',
-        'peringkat_kelayakan',
-        'kaedah_pengendalian',
-        'tarikh_mula',
-        'tarikh_tamat',
-        'id_institusi',
-        'nama_institusi_bm',
-        'nama_institusi_bi',
+        'NoRujProg',
+        'NoSiriSijil',
+        'NamaProgBM',
+        'NamaProgBI',
+        'KodNEC',
+        'BidangProgram',
+        'PeringkatKelayakan',
+        'KaedahPengendalian',
+        'TarikhAkrMula',
+        'TarikhAkrTamat',
+        'IdAgensi',
+        'NamaAgensiBM',
+        'NamaAgensiBI',
     ];
 }
