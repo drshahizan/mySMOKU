3bb6288cf146c2bda7c1dc4c2998ee5aded77cf0
Farahziba <140927300+Farahziba@users.noreply.github.com>
2023-10-24 10:54:12 +0800

    update

--- resources/views/kemaskini/sekretariat/emel/papar_emel.blade.php
+++ resources/views/kemaskini/sekretariat/emel/papar_emel.blade.php
@@ -4,10 +4,10 @@
     <title>{{$emel->subjek}}</title>
 </head>
 <body>
+<h3>{!! nl2br(str_replace(" ", '&nbsp;',$emel->subjek)) !!}</h3>
 <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->pendahuluan)) !!}</p>
 <p>{!! nl2br(str_replace(" ", '&nbsp;', $emel->isi_kandungan1)) !!}</p>
 <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->isi_kandungan2)) !!}</p>
-
 <br>
 <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->penutup)) !!}</p>
 <br>
--- resources/views/permohonan/sekretariat/saringan/email_kembalikan.blade.php
+++ resources/views/permohonan/sekretariat/saringan/email_kembalikan.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{!! nl2br(e($emel->subjek)) !!}</title>
+    <title>{!! nl2br(str_replace(" ", '&nbsp;',$emel->subjek)) !!}</title>
 </head>
 <body>
-    <h3>{!! nl2br(e($emel->subjek)) !!}</h3>
-    <p>{!! nl2br(e($emel->pendahuluan)) !!}</p>
-    <p>{!! nl2br(e($emel->isi_kandungan)) !!}</p>
+    <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->pendahuluan)) !!}</p>
+    <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->isi_kandungan1)) !!}</p>
     @for ($i = 0; $i < count($catatan); $i++)
         {{$i+1}}. {{$catatan[$i]}}. <br>
     @endfor
-
+    <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->isi_kandungan2)) !!}</p>
     <br>
-    <p>{!! nl2br(e($emel->penutup)) !!}</p>
+    <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->penutup)) !!}</p>
     <br>
-    <p>{!! nl2br(e($emel->disediakan_oleh)) !!}</p>
+    <p>{!! nl2br(str_replace(" ", '&nbsp;',$emel->disediakan_oleh)) !!}</p>
 </body>
 </html>
