7ccd5e38370d3bf8652144355032755109bbd5bd
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-10-26 00:04:55 +0800

    emel

--- resources/views/pages/auth/email_reset-password.blade.php
+++ resources/views/pages/auth/email_reset-password.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>BKOKU: Lupa Kata Laluan</title>
+    <title>SistemBKOKU: Lupa Kata Laluan</title>
 </head>
 <body>
-    <h3>SitemBKOKU: Set Semula Kata Laluan Sistem BKOKU dan PPK</h3>
-    <p>Assalammualaikum W.R.T & Salam Sejahtera.</p>
+    <h3>KATALALUAN PENDAFTARAN SISTEM BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)/ PROGRAM PENDIDIKAN KHAS (PPK)</h3>
+    <p>Assalamualaikum WBT & Salam Sejahtera,</p>
+    <br>
+    <p>Tuan/Puan,</p>
     <br>    
-<p>Anda menerima emel ini kerana kami telah menerima permintaan set semula kata laluan untuk akaun anda.</p>
-<p>
-    Set semula kata laluan anda dengan mengklik pautan berikut:
-    <a href="{{ route('password.reset', ['token' => $token]) }}">Set Semula Kata Laluan</a>
-    {{-- {{ route('password.reset', ['token' => $token]) }} --}}
-</p>
-<p>Jika anda tidak meminta set semula kata laluan, tiada tindakan lanjut diperlukan.</p>
-<br>
-<p>Jika ada sebarang pertanyaan, sila hubungi khidmat pelanggan di talian 03-8888 1616
-    atau e-mel ke biasiswabkoku@mohe.gov.my </p>
-
-<p>Untuk maklumat lanjut, sila layari https://biasiswa.mohe.gov.my/ </p>
-<br>
-<p>Sekian, terima kasih</p>
-<br>
-<p><b>Pentadbir Sistem BKOKU dan PPK</b></p>
+    <p>Permohonan lupa kata laluan telah diterima.</p>
+    <p>
+        Set semula kata laluan anda dengan mengklik pautan berikut:
+        <a href="{{ route('password.reset', ['token' => $token]) }}">Set Semula Kata Laluan</a>
+        {{-- {{ route('password.reset', ['token' => $token]) }} --}}
+    </p>
+    <p>Jika anda tidak meminta set semula kata laluan, tiada tindakan lanjut diperlukan.</p>
+    <br>
+    <br>
+    <p>Sekian, terima kasih.</p>
+    <br>
+    <p><b>Unit Bantuan Kewangan OKU/Program Pendidikan Khas</b></p>
+    <p><b>Bahagian Biasiswa</b></p>
+    <p><b>Kementerian Pendidikan Tinggi</b></p>
 </body>
 </html>
--- resources/views/pages/pentadbir/hebahan_iklan.blade.php
+++ resources/views/pages/pentadbir/hebahan_iklan.blade.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>BKOKU: Hebahan Iklan</title>
+    <title>SistemBKOKU: Hebahan Iklan</title>
 </head>
 <body>
-    <h3>SistemBKOKU: Hebahan Iklan Permohonan BKOKU dan PPK</h3>
-    <p>Assalammualaikum W.R.T & Salam Sejahtera.</p>
+    <h3>HEBAHAN IKLAN PERMOHONAN BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)/ PROGRAM PENDIDIKAN KHAS (PPK)</h3>
+    <p>Assalamualaikum WBT & Salam Sejahtera,</p>
+    <br>
+    <p>Tuan/Puan,</p>
+    <br>
+    <p>Untuk makluman :</p>
     <br>
     <p>{!! $catatan !!} 
     <br>
     <br>
     <br>
-    <p>Jika ada sebarang pertanyaan, sila hubungi khidmat pelanggan di talian 03-8888 1616
-        atau e-mel ke biasiswabkoku@mohe.gov.my </p>
+    <p>Jika mempunyai sebarang pertanyaan, sila hubungi <i>hotline</i> BKOKU di talian 03-8888 1616 atau e-mel ke bkoku@mohe.gov.my. </p>
    
     <p>Untuk maklumat lanjut, sila layari https://biasiswa.mohe.gov.my/ </p>
     <br>
-    <p>Sekian, terima kasih</p>
+    <p>Sekian, terima kasih.</p>
     <br>
-    <p><b>Pentadbir Sistem BKOKU dan PPK</b></p>
+    <p><b>Unit Bantuan Kewangan OKU/Program Pendidikan Khas</b></p>
+    <p><b>Bahagian Biasiswa</b></p>
+    <p><b>Kementerian Pendidikan Tinggi</b></p>
 </body>
 </html>
