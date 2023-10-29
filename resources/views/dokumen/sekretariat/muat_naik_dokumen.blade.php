4f91230ed09fa15a69d00f569fe3bb88fe5caa4c
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-10-29 16:25:32 +0800

    update sekretariat upload

--- resources/views/dokumen/sekretariat/muat_naik_dokumen.blade.php
+++ resources/views/dokumen/sekretariat/muat_naik_dokumen.blade.php
@@ -1,7 +1,6 @@
 <x-default-layout> 
 	<head>
 	<!-- MAIN CSS -->
-		<link rel="stylesheet" href="/assets/css/sekretariat.css">
 		<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -9,6 +8,29 @@
 		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 		<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10">
+
+		<style>
+			th{
+				padding-top: 12px!important;
+				padding-bottom: 12px!important;
+				background-color: rgb(35, 58, 108)!important;
+				color: white!important;
+			}
+
+			#file-input-container {
+				display: flex;
+				flex-direction: column;
+				gap: 5px;
+			}
+
+			.file-input {
+				display: flex;
+			}
+
+			.btn {
+				margin-left: 300px;
+			}
+		</style>
 	</head>
 
 	<!--begin::Breadcrumb-->
@@ -36,129 +58,143 @@
 	<br>
 
 	<!--begin::Content-->
-	<div id="kt_app_content" class="app-content flex-column-fluid">
-		<!--begin::Content container-->
-		<div id="kt_app_content_container" class="app-container container-xl" style="width:80%">
-			<!--begin::Layout-->
-			<div class="d-flex flex-column flex-lg-row">
-				<!--begin::Content-->
-				<div class="flex-lg-row-fluid mb-10 mb-lg-0 me-lg-7 me-xl-10">
-					<!--begin::Card-->
-					<div class="card">
-						<!--begin::Card body-->
-						<div class="card-body p-12">
-							<!--begin::Form-->
-							<form action="{{ route('sekretariat.hantar.SPPB') }}" method="post" enctype="multipart/form-data">
-								@csrf
-								<div class="d-flex flex-column align-items-start flex-xl-row">
-									<div class="d-flex flex-center flex-equal fw-row text-nowrap order-1 order-xl-2 me-4" data-bs-toggle="tooltip" data-bs-trigger="hover">
-										<span class="fs-3 fw-bold text-gray-800">Borang Salur Peruntukan Program BKOKU</span>
-									</div>
-								</div>
-								<br>
+<div id="kt_app_content" class="app-content flex-column-fluid">
+    <!--begin::Content container-->
+    <div id="kt_app_content_container" class="app-container container-xl" style="width:90%">
+        <!--begin::Layout-->
+        <div class="d-flex flex-column flex-lg-row">
+            <!--begin::Content-->
+            <div class="flex-lg-row-fluid mb-10 mb-lg-0 me-lg-7 me-xl-10">
+                <!--begin::Card-->
+                <div class="card">
+                    <!--begin::Card body 1-->
+                    <div class="card-body p-20">
+                        <!--begin::Form-->
+                        <form action="{{ route('sekretariat.hantar.SPPB') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="d-flex flex-column align-items-start flex-xl-row">
+                                <div class="d-flex flex-center flex-equal fw-row text-nowrap order-1 order-xl-2 me-4"
+                                    data-bs-toggle="tooltip" data-bs-trigger="hover">
+                                    <span class="fs-3 fw-bold text-gray-800">Borang Salur Peruntukan Program BKOKU</span>
+                                </div>
+                            </div>
+                            <br>
 
-								@if(session('success'))
-									<div class="alert alert-success" style="width: 90%; margin: 0 auto;">
-										{{ session('success') }}
-									</div>
-									<br>
-								@endif
+                            @if(session('success'))
+                            <div class="alert alert-success" style="width: 90%; margin: 0 auto;">
+                                {{ session('success') }}
+                            </div>
+                            <br>
+                            @endif
 
-								@if ($errors->any())
-									<div class="alert alert-danger" style="width: 90%; margin: 0 auto;">
-										@foreach ($errors->all() as $error)
-											<li>{{ $error }}</li>
-										@endforeach
-									</div>
-									<br><br>
-								@endif
-							
-								<table class="table table-bordered table-striped">
-									<thead>
-										<tr>
-											<th>Item</th>
-											<th>Muat Naik Dokumen</th>
-										</tr>
-									</thead>
+                            @if ($errors->any())
+                            <div class="alert alert-danger" style="width: 90%; margin: 0 auto;">
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </div>
+                            <br><br>
+                            @endif
 
-									<tbody>
-										{{-- DOKUMEN 1 --}}
-										<tr>
-											<td>SPBB 1&nbsp;<a href="/assets/contoh/surat_tamat_pengajian__transkrip_akademik.pdf" target="_blank" data-bs-toggle="tooltip" title="contoh"><i class="fa-solid fa-circle-info"></i></a></td>
-											<td>
-												<input type="file" id="dokumen1" name="dokumen1[]" required/>
-												@if(session()->has('uploadedDokumen1'))
-													@foreach(session('uploadedDokumen1') as $doc1)
-														<a href="{{ asset('assets/dokumen/esp/dokumen1/' . $doc1) }}" target="_blank">{{ $doc1 }}</a>
-													@endforeach
-												@endif
-											</td>
-										</tr>
-							
-										{{-- DOKUMEN 2 --}}
-										<tr>
-											<td>SPBB 2&nbsp;<a href="/assets/contoh/surat_tamat_pengajian__transkrip_akademik.pdf" target="_blank" data-bs-toggle="tooltip" title="contoh"><i class="fa-solid fa-circle-info"></i></a></td>
-											<td>
-												<input type="file" id="dokumen2" name="dokumen2[]" required/>
-												@if(session()->has('uploadedDokumen2'))
-													@foreach(session('uploadedDokumen2') as $doc2)
-														<a href="{{ asset('assets/dokumen/esp/dokumen2/' . $doc2) }}" target="_blank">{{ $doc2 }}</a>
-													@endforeach
-												@endif
-											</td>
-										</tr>
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 25%">Item</th>
+                                        <th style="width: 75%">Pilih</th>
+                                    </tr>
+                                </thead>
 
-										{{-- DOKUMEN 3 --}}
-										<tr>
-											<td>SPBB 3&nbsp;<a href="/assets/contoh/surat_tamat_pengajian__transkrip_akademik.pdf" target="_blank" data-bs-toggle="tooltip" title="contoh"><i class="fa-solid fa-circle-info"></i></a></td>
-											<td>
-												<input type="file" id="dokumen3" name="dokumen3[]" required/>
-												@if(session()->has('uploadedDokumen3'))
-													@foreach(session('uploadedDokumen3') as $doc3)
-														<a href="{{ asset('assets/dokumen/esp/dokumen3/' . $doc3) }}" target="_blank">{{ $doc3 }}</a>
-													@endforeach
-												@endif
-											</td>
-										</tr>
-							
-										<tr>
-											<td colspan="2">
-												<div class="text-dark fw-semibold fs-6"><i class='fas fa-exclamation-triangle' style='color:orange; font-size:15px;'></i>&nbsp;
-													Gunakan kemudahan <a href="https://compressjpeg.com/" target="_blank">di sini</a> untuk mengurangkan saiz fail sebelum memuat naik fail.</a>
+                                <tbody>
+									{{-- NAMA INSTITUSI --}}
+                                    <tr>
+										<td>Nama Institusi</td>
+                                        <td>
+                                            <select name="institusi" style="padding: 5px;">
+                                                <option value="">Pilih Institusi Pengajian</option>
+                                                @foreach ($institusiPengajian as $institusi)
+                                                <option value="{{ $institusi->id_institusi }}"
+                                                    {{ Request::get('institusi') == $institusi->id_institusi ? 'selected' : '' }}>
+                                                    {{ $institusi->nama_institusi }}</option>
+                                                @endforeach
+                                            </select>
+                                        </td>
+                                    </tr>
+
+									{{-- DOKUMEN SPPB --}}
+									<tr>
+										<td>Dokumen SPBB</td>
+										<td>
+											<div id="file-input-container">
+												<!-- File input fields for SPPB1 -->
+												<div class="d-flex">
+													<div class="file-input">
+														<input type="file" name="dokumen1[]" required multiple />
+													</div>
+													<button id="add-file-input" class="btn btn-warning btn-sm" style="font-size: 12px; padding: 4px 10px;">+</button>
 												</div>
-							
-												<div class="text-dark fw-semibold fs-6"><i class='fas fa-info-circle' style='color:gray; font-size:15px;'></i>&nbsp; 
-													Format fail yang boleh dimuat naik adalah format '.pdf', '.xls', '.xlsx'.
-												</div>
-							
-												<div class="text-dark fw-semibold fs-6"><i class='fas fa-info-circle' style='color:gray; font-size:15px;'></i>&nbsp; 
-													Saiz maksimum fail adalah 2 MB.
-												</div>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							
-								<div class="modal-footer flex-center">
-									<button type="submit" id="kt_modal_add_customer_submit" class="btn btn-primary">
-										<span class="indicator-label">Hantar</span>
-										<span class="indicator-progress">Sila tunggu...
-											<span class="spinner-border spinner-border-sm align-middle ms-2"></span>
-										</span>
-									</button>
-								</div>
-							</form>							
-							<!--end::Form-->
-						</div>
-						<!--end::Card body-->
+											</div>																						
+                                        </td>
+									</tr>
+                                    
+                                    <tr>
+                                        <td colspan="2">
+                                            <div class="text-dark fw-semibold fs-6"><i class='fas fa-exclamation-triangle' style='color:orange; font-size:15px;'></i>&nbsp;
+                                                Gunakan kemudahan <a href="https://compressjpeg.com/" target="_blank">di sini</a>
+                                                untuk mengurangkan saiz fail sebelum memuat naik fail.</a>
+                                            </div>
+
+                                            <div class="text-dark fw-semibold fs-6"><i class='fas fa-info-circle' style='color:gray; font-size:15px;'></i>&nbsp;
+                                                Format fail yang boleh dimuat naik adalah format '.pdf', '.xls', '.xlsx'.
+                                            </div>
+
+                                            <div class="text-dark fw-semibold fs-6"><i class='fas fa-info-circle' style='color:gray; font-size:15px;'></i>&nbsp;
+                                                Saiz maksimum fail adalah 2 MB.
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <div class="modal-footer flex-center">
+                                <button type="submit" id="kt_modal_add_customer_submit" class="btn btn-primary">
+									<span class="indicator-label">Hantar</span>
+								</button>
+							</div>
+						</form>
 					</div>
-					<!--end::Card-->
+					<!--end::Card body 1-->
 				</div>
-				<!--end::Content-->
 			</div>
-			<!--end::Layout-->
 		</div>
-		<!--end::Content container-->
 	</div>
-	<!--end::Content-->
+						
+	<script>
+		document.addEventListener("DOMContentLoaded", function () {
+			const fileInputContainer = document.getElementById("file-input-container");
+			const addFileInputButton = document.getElementById("add-file-input");
+	
+			let fileInputIndex = 2; // Start with 2 for SPPB2
+	
+			addFileInputButton.addEventListener("click", function () {
+				// Create a new file input container
+				const newFileInputContainer = document.createElement("div");
+				newFileInputContainer.classList.add("file-input");
+	
+				// Create a new file input field
+				const newInput = document.createElement("input");
+				newInput.type = "file";
+				newInput.name = `dokumen${fileInputIndex}[]`;
+				newInput.required = true;
+				newInput.multiple = true;
+	
+				// Append the new file input to the container
+				newFileInputContainer.appendChild(newInput);
+	
+				// Append the new container to the file input container
+				fileInputContainer.appendChild(newFileInputContainer);
+	
+				fileInputIndex++;
+			});
+		});
+	</script>				
 </x-default-layout>
