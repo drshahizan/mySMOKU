2c1f8fa9eca4ab16525f048d3cbd6ce7aca4da8c
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-11-01 21:11:13 +0800

    dah pull hahaha

---
+++ resources/views/dokumen/penyelaras/muat_turun_dokumen.blade.php
@@ -0,0 +1,112 @@
+<x-default-layout> 
+
+<head>
+<!-- MAIN CSS -->
+	<link rel="stylesheet" href="/assets/css/sekretariat.css">
+	<script src="https://cdn.tiny.cloud/1/v736541al0ntzh14edk63z19dzyqs1xn2bkc5em78rv1yeis/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+	<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<!--begin::Page title-->
+	<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+		<!--begin::Title-->
+		<h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Penyaluran</h1>
+		<!--end::Title-->
+		<!--begin::Breadcrumb-->
+		<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+			<!--begin::Item-->
+			<li class="breadcrumb-item text-dark" style="color:darkblue">Muat Turun</li>
+			<!--end::Item-->
+			<!--begin::Item-->
+			<li class="breadcrumb-item">
+				<span class="bullet bg-gray-400 w-5px h-2px"></span>
+			</li>
+			<!--end::Item-->
+			<!--begin::Item-->
+			<li class="breadcrumb-item text-dark" style="color:darkblue">Dokumen ESP</li>
+			<!--end::Item-->
+		</ul>
+	<!--end::Breadcrumb-->
+	</div>
+
+	<br>
+
+	<!-- Main body part  -->
+	<div id="main-content">
+		<div class="container-fluid">
+			<div class="block-header">
+				<div class="row clearfix">
+					<div class="card p-10">
+						<div class="header">
+							<h2>Borang Salur Peruntukan Program BKOKU<br><small>Sila klik ikon lihat untuk lihat kesemua dokumen yang dimuat naik.</small></h2>
+						</div>
+
+						<div class="body">
+							<div class="table-responsive" id="table-responsive">
+								<table id="sortTable1" class="table table-bordered table-striped">
+									<thead>
+										<tr>
+											<th class="text-center" style="width: 5%">No</th>
+											<th class="text-center" style="width: 60%">Institusi</th>
+											<th class="text-center" style="width: 20%">Tarikh Dokumen Dikemaskini</th>
+											<th class="text-center" style="width: 15%">Dokumen</th>
+										</tr>
+									</thead>
+							
+									<tbody>
+										@php
+										$i = 0;
+										@endphp
+	
+										@foreach ($dokumen as $doc)
+											@php
+												$id = $doc->institusi_id;
+												$nama_institusi = DB::table('bk_info_institusi')->where('id_institusi', $id)->value('nama_institusi');
+											@endphp
+											<tr>
+												<td class="text-center" data-no="{{ $i++ }}">{{ $i }}</td>
+												<td>{{ $nama_institusi }}</td>
+												<td class="text-center">{{date('d/m/Y', strtotime($doc->updated_at))}}</td>
+												<td class="text-center">
+													<a href="{{ asset('assets/dokumen/esp/dokumen1/' . $doc->dokumen) }}" class="btn btn-info btn-sm" style="width: 80%; margin: 0 auto;">
+														Lihat <i class='fas fa-eye' style='color:white; padding-left:20px;'></i>
+													</a>
+												</td>
+											</tr>
+											@endforeach
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script>
+		$(document).ready(function() {
+			var table = $('#sortTable1').DataTable({
+				"columnDefs": [
+					{
+						"targets": 'no-sort',
+						"orderable": false
+					}
+				],
+			});
+
+			// Disable sorting for the "No" column
+			table.on('order.dt', function() {
+				table.column(0, { order: 'applied' }).nodes().each(function(cell, i) {
+					cell.innerHTML = i + 1;
+				});
+			}).draw();
+		});
+	</script>
+</body>
+	
+</x-default-layout>
