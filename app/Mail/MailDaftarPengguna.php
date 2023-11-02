7ba0fa8b308fb87dd2c38559e72a7095c3eca28d
wansyafiqah <45482812+wansyafiqah@users.noreply.github.com>
2023-11-02 13:54:07 +0800

    Add files via upload

---
+++ app/Mail/MailDaftarPengguna.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+
+class maildaftarPengguna extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $email;
+    public $no_kp;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($email, $no_kp)
+    {
+        $this->email = $email;
+        $this->no_kp = $no_kp;
+    }
+
+    /**
+     * Build new message instance.
+     */
+    public function build()
+    {
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60), // Adjust the expiration time as needed
+            ['id' => $this->no_kp, 'hash' => sha1($this->email)]
+        );
+
+        $subject = "Daftar Pengguna Sistem BKOKU";
+        return $this->subject($subject)
+                    ->view('pages.pentadbir.emel-daftar')
+                    ->with([
+                        'email' => $this->email,
+                        'no_kp' => $this->no_kp,
+                        'verificationUrl' => $verificationUrl,
+                    ]);
+                    
+    }
+}
