85f48e601eacc7f90120cc3d374585530360c003
wansyafiqah <45482812+wansyafiqah@users.noreply.github.com>
2023-11-19 18:43:07 +0800

    Add files via upload

---
+++ public/assets/js/custom/authentication/semak/general.js
@@ -0,0 +1,321 @@
+"use strict";
+
+// Class definition
+var KTSignupGeneral = function () {
+    // Elements
+    var form;
+    var submitButton;
+    var validator;
+    var passwordMeter;
+
+    // Handle form
+    var handleForm = function (e) {
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+        validator = FormValidation.formValidation(
+            form,
+            {
+                fields: {
+                    'id_institusi': {
+                        validators: {
+                            notEmpty: {
+                                message: 'First Name is required'
+                            }
+                        }
+                    },
+                    
+                    'peringkat_pengajian': {
+                        validators: {
+                            notEmpty: {
+                                message: 'First Name is required'
+                            }
+                        }
+                    },
+                    'nama_kursus': {
+                        validators: {
+                            notEmpty: {
+                                message: 'The password is required'
+                            },
+                            callback: {
+                                message: 'Please enter valid password',
+                                callback: function (input) {
+                                    if (input.value.length > 0) {
+                                        return validatePassword();
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger({
+                        event: {
+                            password: false
+                        }
+                    }),
+                    bootstrap: new FormValidation.plugins.Bootstrap5({
+                        rowSelector: '.fv-row',
+                        eleInvalidClass: '',  // comment to enable invalid state icons
+                        eleValidClass: '' // comment to enable valid state icons
+                    })
+                }
+            }
+        );
+
+        // Handle form submit
+        submitButton.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            validator.revalidateField('password');
+
+            validator.validate().then(function (status) {
+                if (status == 'Valid') {
+                    // Show loading indication
+                    submitButton.setAttribute('data-kt-indicator', 'on');
+
+                    // Disable button to avoid multiple click
+                    submitButton.disabled = true;
+
+                    // Simulate ajax request
+                    setTimeout(function () {
+                        // Hide loading indication
+                        submitButton.removeAttribute('data-kt-indicator');
+
+                        // Enable button
+                        submitButton.disabled = false;
+
+                        // Show message popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                        Swal.fire({
+                            text: "You have successfully reset your password!",
+                            icon: "success",
+                            buttonsStyling: false,
+                            confirmButtonText: "Ok, got it!",
+                            customClass: {
+                                confirmButton: "btn btn-primary"
+                            }
+                        }).then(function (result) {
+                            if (result.isConfirmed) {
+                                form.reset();  // reset form
+                                passwordMeter.reset();  // reset password meter
+                                //form.submit();
+
+                                //form.submit(); // submit form
+                                var redirectUrl = form.getAttribute('data-kt-redirect-url');
+                                if (redirectUrl) {
+                                    location.href = redirectUrl;
+                                }
+                            }
+                        });
+                    }, 1500);
+                } else {
+                    // Show error popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                    Swal.fire({
+                        text: "Sorry, looks like there are some errors detected, please try again.",
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok, got it!",
+                        customClass: {
+                            confirmButton: "btn btn-primary"
+                        }
+                    });
+                }
+            });
+        });
+
+        // Handle password input
+        form.querySelector('input[name="password"]').addEventListener('input', function () {
+            if (this.value.length > 0) {
+                validator.updateFieldStatus('password', 'NotValidated');
+            }
+        });
+    }
+
+
+    // Handle form ajax
+    var handleFormAjax = function (e) {
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+        validator = FormValidation.formValidation(
+            form,
+            {
+                fields: {
+                    'id_institusi': {
+                        validators: {
+                            notEmpty: {
+                                message: 'Pilih Pusat Pengajian'
+                            }
+                        }
+                    },
+                    'peringkat_pengajian': {
+                        validators: {
+                            notEmpty: {
+                                message: 'Pilih Peringkat Pengajian'
+                            }
+                        }
+                    },
+                    'nama_kursus': {
+                        validators: {
+                            notEmpty: {
+                                message: 'Pilih Nama Kursus'
+                            },
+                        }
+                    },
+                    
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger({
+                        event: {
+                            password: false
+                        }
+                    }),
+                    bootstrap: new FormValidation.plugins.Bootstrap5({
+                        rowSelector: '.fv-row',
+                        eleInvalidClass: '',  // comment to enable invalid state icons
+                        eleValidClass: '' // comment to enable valid state icons
+                    })
+                }
+            }
+        );
+
+        // Handle form submit
+        submitButton.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            validator.revalidateField('password');
+
+            validator.validate().then(function (status) {
+                if (status == 'Valid') {
+                    // Show loading indication
+                    submitButton.setAttribute('data-kt-indicator', 'on');
+
+                    // Disable button to avoid multiple click
+                    submitButton.disabled = true;
+
+
+                    // Check axios library docs: https://axios-http.com/docs/intro
+                    axios.post(submitButton.closest('form').getAttribute('action'), new FormData(form)).then(function (response) {
+                        if (response) {
+                            // Check if the user chose 'Ya'
+                            const terimHLPValue = form.querySelector('input[name="terimHLP"]:checked').value;
+                            const cutiValue = form.querySelector('input[name="cuti"]:checked').value;
+
+                            if (terimHLPValue === 'ya' || cutiValue === 'ya') {
+                                let errorMessage = "Anda tidak layak daftar kerana anda penerima ";
+                                if (terimHLPValue === 'ya') {
+                                    errorMessage += "HLP";
+                                }
+                                if (cutiValue === 'ya') {
+                                    errorMessage += (terimHLPValue === 'ya' ? ' dan ' : '') + "Cuti Belajar Bergaji Penuh";
+                                }
+                            
+                                Swal.fire({
+                                    text: errorMessage,
+                                    icon: "error",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "Ok",
+                                    customClass: {
+                                        confirmButton: "btn btn-primary",
+                                    },
+                                    didClose: () => {
+                                        // Redirect to the login page
+                                        window.location.href = '/login';
+                                    }
+                                });
+                            } else {
+                                form.reset();
+
+                                const redirectUrl = form.getAttribute('data-kt-redirect-url');
+
+                                if (redirectUrl) {
+                                    location.href = redirectUrl;
+                                }
+                            }    
+                        } else {
+                            // Show error popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                            Swal.fire({
+                                text: "Sorry, looks like there are some errors detected, please try again.",
+                                icon: "error",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok, got it!",
+                                customClass: {
+                                    confirmButton: "btn btn-primary"
+                                }
+                            });
+                        }
+                    }).catch(function (error) {
+                        Swal.fire({
+                            text: "Sorry, looks like there are some errors detected, please try again.",
+                            icon: "error",
+                            buttonsStyling: false,
+                            confirmButtonText: "Ok, got it!",
+                            customClass: {
+                                confirmButton: "btn btn-primary"
+                            }
+                        });
+                    }).then(() => {
+                        // Hide loading indication
+                        submitButton.removeAttribute('data-kt-indicator');
+
+                        // Enable button
+                        submitButton.disabled = false;
+                    });
+
+                } else {
+                    // Show error popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                    Swal.fire({
+                        text: "Sila isi maklumat yang diperlukan",
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok",
+                        customClass: {
+                            confirmButton: "btn btn-primary"
+                        }
+                    });
+                }
+            });
+        });
+
+        // Handle password input
+        form.querySelector('input[name="password"]').addEventListener('input', function () {
+            if (this.value.length > 0) {
+                validator.updateFieldStatus('password', 'NotValidated');
+            }
+        });
+    }
+
+
+    // Password input validation
+    var validatePassword = function () {
+        return (passwordMeter.getScore() > 50);
+    }
+
+    var isValidUrl = function(url) {
+        try {
+            new URL(url);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // Public functions
+    return {
+        // Initialization
+        init: function () {
+            // Elements
+            form = document.querySelector('#kt_sign_up_form');
+            submitButton = document.querySelector('#kt_sign_up_submit');
+            passwordMeter = KTPasswordMeter.getInstance(form.querySelector('[data-kt-password-meter="true"]'));
+
+            if (isValidUrl(submitButton.closest('form').getAttribute('action'))) {
+                handleFormAjax();
+            } else {
+                handleForm();
+            }
+        }
+    };
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function () {
+    KTSignupGeneral.init();
+});
