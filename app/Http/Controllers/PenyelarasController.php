ab2c20cc49caf9c4763eae571d4c63fc256dd593
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-11-20 15:02:35 +0800

    Update PenyelarasController.php

--- app/Http/Controllers/PenyelarasController.php
+++ app/Http/Controllers/PenyelarasController.php
@@ -36,8 +36,11 @@ use App\Models\SejarahTuntutan;
 use App\Models\Peperiksaan;
 use App\Models\Saringan;
 use App\Models\Kelulusan;
+use App\Models\Keturunan;
 use App\Models\MaklumatBank;
 use Carbon\Carbon;
+use DateTime;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
@@ -64,6 +67,122 @@ class PenyelarasController extends Controller
 
     public function store(Request $request)
     {
+
+        //using api smoku
+        /*
+        $request->validate([
+            'no_kp' => ['required', 'string'],
+            
+        ]);
+        $nokp_in = $request->no_kp;
+        
+
+        $headers = [
+            'Cache-Control' => 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer knhnxYoATGLiN5WxErU6SVVw8c9xhw09vQ3KRPkOtcH3O0CYh21wDA4CsypX',
+        ];
+        $client = new Client();
+        $url = 'https://oku-staging.jkm.gov.my/api/oku/' . $request->no_kp;
+        $request = $client->get($url, ['headers' => $headers]);
+
+        $response = $request ? $request->getBody()->getContents() : null;
+        $status = $request ? $request->getStatusCode() : 500;
+
+        // Parse the JSON string
+        $data = json_decode($response, true);
+        
+
+        // Access the "data" field
+        $dataField = [];
+        if (isset($data['data'])) {
+            $dataField = $data['data'];
+            
+            // Now, $dataField contains the "data" array
+            $no_kp = $dataField['NO_ID'];
+           
+            $jantina = isset($dataField['JANTINA']) ? substr($dataField['JANTINA'], 0, 1) : null;
+            $tarikh_lahir = $dataField['TARIKH_LAHIR'];
+            $tarikhLahirDate = DateTime::createFromFormat('d/m/Y', $tarikh_lahir);
+            $formattedDate = $tarikhLahirDate->format('Y-m-d');
+
+            $kategori = $dataField['KATEGORI'];
+            $kod_oku = JenisOku::where('kecacatan',$kategori)->first();
+
+            $keturunan = $dataField['KETURUNAN'];
+            $kod = Keturunan::where('keturunan',$keturunan)->first();
+            if ($kod !== null) {
+                $kod_keturunan = $kod->kod_keturunan;
+            } else {
+                $kod_keturunan = null;
+            }
+
+            $hubungan = $dataField['HUBUNGAN_WARIS'];
+            $kod = Hubungan::where('hubungan',$hubungan)->first();
+            if ($kod !== null) {
+                $kod_hubungan = $kod->kod_hubungan;
+            } else {
+                $kod_hubungan = null;
+            }
+            //dd($kod_hubungan);
+
+            Smoku::updateOrInsert(
+                ['no_kp' => $dataField['NO_ID']], // Condition to find the record
+                [
+                    'no_id_tentera' => $dataField['NO_ID_TENTERA'],
+                    'nama' => $dataField['NAMA_PENUH'],
+                    'no_daftar_oku' => $dataField['NO_DAFTAR_OKU'],
+                    'kategori' => $kod_oku->kod_oku,
+                    'jantina' => $jantina,
+                    'tarikh_lahir' => $formattedDate,
+                    'umur' => $dataField['UMUR'],
+                    'keturunan' => $kod_keturunan,
+                    'tel_rumah' => $dataField['TEL_RUMAH'],
+                    'tel_bimbit' => $dataField['TEL_BIMBIT'],
+                    'email' => $dataField['EMEL'],
+                    'pekerjaan' => $dataField['PEKERJAAN'],
+                    'pendapatan' => $dataField['PENDAPATAN'],
+                    'status_pekerjaan' => $dataField['STATUS_PEKERJAAN'],
+                    'alamat_tetap' => $dataField['ALAMAT_TETAP'],
+                    'alamat_surat_menyurat' => $dataField['ALAMAT_SURAT_MENYURAT'],
+                    'nama_waris' => $dataField['NAMA_WARIS'],
+                    'tel_bimbit_waris' => $dataField['TEL_BIMBIT_WARIS'],
+                    'hubungan_waris' => $kod_hubungan,
+                    'pekerjaan_waris' => $dataField['PEKERJAAN_WARIS'],
+                    // 'pendapatan_waris' => $dataField['PENDAPATAN_WARIS'],
+                    'updated_at' => DB::raw('NOW()')
+                ],
+                [
+                    'created_at' => DB::raw('NOW()')
+                ]
+            );
+
+            $smoku=Smoku::where([['no_kp', '=', $no_kp]])->first();
+
+            if ($smoku != null) {
+                DB::table('smoku_penyelaras')->insert([
+                    'smoku_id' => $smoku->id,
+                    'penyelaras_id' => Auth::user()->id,
+                    'status' => '1',
+                    'created_at' => now(), // sebab tak guna model
+                    'updated_at' => now(),
+                ]);
+                
+               
+            }
+            
+            return redirect()->route('penyelaras.dashboard')->with($no_kp)
+            ->with('success', $no_kp. ' Sah sebagai OKU berdaftar dengan JKM.');
+
+        } else {
+
+            return redirect()->route('penyelaras.dashboard')
+            ->with('failed', $nokp_in. ' Bukan OKU yang berdaftar dengan JKM.');
+        }
+        */
+        
+
+        
         $request->validate([
             'no_kp' => ['required', 'string'],
         ]);
@@ -91,6 +210,8 @@ class PenyelarasController extends Controller
                 return redirect()->route('penyelaras.dashboard')
                 ->with('failed', $nokp_in. ' Bukan OKU yang berdaftar dengan JKM.');
             }
+
+        
     }
 
     public function deletePendaftaran($id)
