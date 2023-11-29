0291d8507d50fcc98fbf5b8c6f446bec7f389a29
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-11-29 17:52:30 +0800

    update import

--- app/Imports/ModifiedTuntutanImport.php
+++ app/Imports/ModifiedTuntutanImport.php
@@ -4,7 +4,7 @@ namespace App\Imports;
 
 use Carbon\Carbon;
 use App\Models\Tuntutan;
-use App\Models\SejarahPermohonan;
+use App\Models\SejarahTuntutan;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -21,7 +21,7 @@ class ModifiedTuntutanImport implements ToCollection, WithHeadingRow
         foreach ($rows as $row) {
             // Extract specific columns
             $this->modifiedData[] = [
-                'no_rujukan_permohonan' => $row['id_permohonan'],
+                'no_rujukan_tuntutan' => $row['id_tuntutan'],
                 'yuran_dibayar' => number_format($row['yuran_dibayar'], 2, '.', ''),
                 'wang_saku_dibayar' => number_format($row['wang_saku_dibayar'], 2, '.', ''),
                 'no_baucer' => $row['no_baucer'],
@@ -29,7 +29,7 @@ class ModifiedTuntutanImport implements ToCollection, WithHeadingRow
                 'tarikh_baucer' => $this->convertExcelDate($row['tarikh_baucer']),
             ];
         }
-        // Update the 'status' column in the permohonan table to 8
+        // Update the 'status' column in the tuntutan table to 8
         $this->updateStatus();
     }
 
@@ -46,23 +46,23 @@ class ModifiedTuntutanImport implements ToCollection, WithHeadingRow
 
     private function updateStatus()
     {
-        // Get an array of unique 'no_rujukan_permohonan' values from the modified data
-        $noRujukanArray = collect($this->modifiedData)->pluck('no_rujukan_permohonan')->unique()->toArray();
+        // Get an array of unique 'no_rujukan_tuntutan' values from the modified data
+        $noRujukanArray = collect($this->modifiedData)->pluck('no_rujukan_tuntutan')->unique()->toArray();
 
-        // Update the 'status' column to 8 for the permohonan records with matching 'no_rujukan_permohonan'
-        Tuntutan::whereIn('no_rujukan_permohonan', $noRujukanArray)
+        // Update the 'status' column to 8 for the tuntutan records with matching 'no_rujukan_tuntutan'
+        Tuntutan::whereIn('no_rujukan_tuntutan', $noRujukanArray)
             ->update(['status' => 8]);
 
-        // Fetch the corresponding rows from permohonan table
-        $permohonans = Tuntutan::whereIn('no_rujukan_permohonan', $noRujukanArray)
+        // Fetch the corresponding rows from tuntutan table
+        $tuntutans = Tuntutan::whereIn('no_rujukan_tuntutan', $noRujukanArray)
         ->select('id', 'smoku_id')
         ->get();
 
-        // Create new records in sejarah_permohonan table based on the fetched rows
-        foreach ($permohonans as $permohonan) {
-            SejarahPermohonan::create([
-                'permohonan_id' => $permohonan->id,
-                'smoku_id' => $permohonan->smoku_id,
+        // Create new records in sejarah_tuntutan table based on the fetched rows
+        foreach ($tuntutans as $tuntutan) {
+            SejarahTuntutan::create([
+                'tuntutan_id' => $tuntutan->id,
+                'smoku_id' => $tuntutan->smoku_id,
                 'status' => 8
             ]);
         }
