d2e7de4d407bc04dbfd1857e9477faf7a601b44f
nfarahz <norfarahziba@graduate.utm.my>
2023-12-14 14:39:57 +0800

    Update TaskUnlock.php

--- app/Console/Commands/TaskUnlock.php
+++ app/Console/Commands/TaskUnlock.php
@@ -3,8 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Models\Permohonan;
+use App\Models\SejarahPermohonan;
+use App\Models\SejarahTuntutan;
+use App\Models\Tuntutan;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
 
 class TaskUnlock extends Command
 {
@@ -13,7 +17,7 @@ class TaskUnlock extends Command
      *
      * @var string
      */
-    protected $signature = 'app:task-unlock';
+    protected $signature = 'task:unlock';
 
     /**
      * The console command description.
@@ -30,14 +34,41 @@ class TaskUnlock extends Command
         $permohonan = Permohonan::where('status', 3)->get();
         $now = Carbon::now();
         foreach ($permohonan as $item){
-            $time_passed = $now->diffInHours($item['updated_at']);
-            if($time_passed > 24){
+            $time_passed = $now->diffInMinutes($item['updated_at']);
+            if($time_passed > 1440){
                 Permohonan::where('id', $item['id'])
                     ->update([
                         'status'   =>  2,
                     ]);
+
+                $status_rekod = new SejarahPermohonan([
+                    'smoku_id'          =>  $item['smoku_id'],
+                    'permohonan_id'     =>  $item['id'],
+                    'status'            =>  2,
+                ]);
+                $status_rekod->save();
             }
         }
+
+        $tuntutan = Tuntutan::where('status', 3)->get();
+        $now = Carbon::now();
+        foreach ($tuntutan as $item){
+            $time_passed = $now->diffInHours($item['updated_at']);
+            if($time_passed > 24){
+                Tuntutan::where('id', $item['id'])
+                    ->update([
+                        'status'   =>  2,
+                    ]);
+
+                $status_rekod = new SejarahTuntutan([
+                    'smoku_id'          =>  $item['smoku_id'],
+                    'tuntutan_id'       =>  $item['id'],
+                    'status'            =>  2,
+                ]);
+                $status_rekod->save();
+            }
+        }
+
         $this->info('task executed successfully!');
     }
 }
