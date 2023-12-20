0ebed6cf6bd69421d0d5665a52c6cd53f4b7c636
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2023-12-20 12:10:08 +0800

    update

--- resources/views/esp/permohonan/permohonan_esp.blade.php
+++ resources/views/esp/permohonan/permohonan_esp.blade.php
@@ -51,7 +51,7 @@
           <div class="card">
             <!--begin::Card header-->
             <div class="header">
-                <h2>Senarai Permohonan Layak</h2><br><small>Sila tanda pada kotak kecik dan klik "Hantar" untuk menyerahkan data pemohon kepada ESP.</small>
+                <h2>Senarai Permohonan Layak<br><small>Sila tanda pada kotak kecik dan klik "Hantar" untuk menyerahkan data pemohon kepada ESP.</small></h2>
             </div>
             <!--end::Card header-->
 
