aaafb1faa90b664be16d7d4fe15c3bfe5a4cf420
Wansyafiqah <wnsyafiqah4@gmail.com>
2023-12-23 12:23:02 +0800

    Update _user-account-menu.blade.php

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -1,38 +1,32 @@
 <!--begin::User account menu-->
-<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
+<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-300px" data-kt-menu="true">
     <!--begin::Menu item-->
     <div class="menu-item px-3">
         <div class="menu-content d-flex align-items-center px-3">
             @php
-                $nama = DB::table('smoku')->where('no_kp', Auth::user()->no_kp)->value('nama');
-                $nama2 = Auth::user()->nama;
+                $nama = Auth::user()->nama;
+                $user = DB::table('users')->where('no_kp', Auth::user()->no_kp)->first();
+                $tahap = DB::table('roles')->where('id',$user->tahap)->value('name');
+                if($user->tahap == 2 || $user->tahap == 6){
+                    $institusi = DB::table('bk_info_institusi')->where('id_institusi',$user->id_institusi)->value('nama_institusi');
+                }
             @endphp
             <!--begin::Avatar-->
             <div class="symbol symbol-50px me-5">
-                @if($nama2)
-                    <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle($nama2) }}" style="color:#3d0066; background-color:#e2cfea">
-                        {{ substr($nama2,0,1) }}
-                    </div>
-                @elseif($nama)
-                    <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle($nama) }}" style="color:#3d0066; background-color:#e2cfea">
-                        {{ substr($nama,0,1) }}
-                    </div>
-                @else
-                    <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle(Auth::user()->email) }}" style="color:#3d0066; background-color:#e2cfea">
-                        {{ substr(Auth::user()->email,0,1) }}
-                    </div>
-                @endif
+                <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle($nama) }}" style="color:#3d0066; background-color:#e2cfea">
+                    {{ substr($nama,0,1) }}
+                </div>
             </div>
             <!--end::Avatar-->
 
             <!--begin::Username-->
             <div class="d-flex flex-column">
-                @if($nama2)
-                    <div class="fw-bold d-flex align-items-center fs-5">{{$nama2}}</div>
-                @else
-                    <div class="fw-bold d-flex align-items-center fs-5">{{$nama}}</div>
+                <div class="fw-bold d-flex align-items-center fs-5">{{$nama}}</div>
+                <div class="fw-semibold text-muted fs-7">{{Auth::user()->email}}</div>
+                <div class="fw-bold d-flex align-items-center fs-7">{{strtoupper($tahap)}}</div>
+                @if($user->tahap == 2 || $user->tahap == 6)
+                <div class="fw-bold d-flex align-items-center fs-7">{{strtoupper($institusi)}}</div>
                 @endif
-                <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
             </div>
             <!--end::Username-->
         </div>
