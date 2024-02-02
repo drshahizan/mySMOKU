b9ebda636bccda615f0d621d3c7cbafaeaf73c90
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-02-02 12:52:45 +0800

    Update _auth.blade.php

--- resources/views/layout/_auth.blade.php
+++ resources/views/layout/_auth.blade.php
@@ -23,7 +23,7 @@
             <!--end::Body-->
 
             <!--begin::Aside-->
-            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ image('misc/auth-bg.jpg') }})">
+            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ image('misc/login2.jpeg') }})">
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!-- Announcement Content -->
