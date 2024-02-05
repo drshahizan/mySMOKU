7ffae745199c1eb9045c1c826dd479b23ba54faf
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-02-05 15:58:13 +0800

    Update _menu.blade.php

--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -1,15 +1,30 @@
-@if(Auth::user()->tahap=='1')
-@php
-	$smoku_id = DB::table('smoku')->where('no_kp',Auth::user()->no_kp)->first();
-	$permohonan = DB::table('permohonan')->where('smoku_id', $smoku_id->id)->first();
-	$akademik = DB::table('smoku_akademik')->where('smoku_id', $smoku_id->id)->where('status', 1)->first();
-	$institusi = DB::table('bk_info_institusi')->where('id_institusi', $akademik->id_institusi)->first();
-	// dd($institusi);
-@endphp
 <!--begin::sidebar menu-->
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
 	<!--begin::Menu wrapper-->
 	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+
+	{{-- PELAJAR UA/IPTS/PPK --}}
+	@if(Auth::user()->tahap=='1')
+		@php
+			$smoku_id = DB::table('smoku')->where('no_kp',Auth::user()->no_kp)->first();
+			$permohonan = DB::table('permohonan')->where('smoku_id', $smoku_id->id)->first();
+			$akademik = DB::table('smoku_akademik')->where('smoku_id', $smoku_id->id)->where('status', 1)->first();
+			$institusi = DB::table('bk_info_institusi')->where('id_institusi', $akademik->id_institusi)->first();
+
+			// Retrieve data from bk_tarikh_iklan table
+			$bk_tarikh_iklan = DB::table('bk_tarikh_iklan')->first();
+
+			// Get current date and time
+			$currentDateTime = now();
+
+			// Check if current date and time fall within the allowed range
+			$tarikhMula = \Carbon\Carbon::parse($bk_tarikh_iklan->tarikh_mula . ' ' . $bk_tarikh_iklan->masa_mula);
+			$tarikhTamat = \Carbon\Carbon::parse($bk_tarikh_iklan->tarikh_tamat . ' ' . $bk_tarikh_iklan->masa_tamat);
+
+			// Check if current date and time fall within the allowed range
+			$isWithinRange = $currentDateTime->between($tarikhMula, $tarikhTamat);
+		@endphp
+		
 		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
 			<!--begin:Menu item-->
@@ -47,12 +62,21 @@
 				</div>
 
 				@if($institusi->jenis_institusi === 'IPTS')
-					<div class="menu-item">
-						<a class="menu-link" href="{{ route('permohonan') }}">
-							<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
-							<span class="menu-title">Baharu</span>
-						</a>
-					</div>
+					@if($isWithinRange)
+						<div class="menu-item">
+							<a class="menu-link" href="{{ route('permohonan') }}">
+								<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
+								<span class="menu-title">Baharu</span>
+							</a>
+						</div>							
+					@else
+						<div class="menu-item">
+							<a class="menu-link" href="#" onclick="showAlert()">
+								<span class="menu-icon">{!! getIcon('book', 'fs-2') !!}</span>
+								<span class="menu-title">Baharu</span>
+							</a>
+						</div>
+					@endif
 				@endif
 
 				<div class="menu-item">
@@ -88,134 +112,128 @@
 					</div>
 				@endif
 			@endif
-
 		</div>
-
 		<!--end::Menu-->
 
-@elseif(Auth::user()->tahap=='2')
-@php
-	$user = DB::table('users')->where('no_kp',Auth::user()->no_kp)->first();
-	$institusi = DB::table('bk_info_institusi')->where('id_institusi', $user->id_institusi)->first();
-	// dd($institusi->jenis_institusi);
-@endphp
-<!--begin::sidebar menu-->
-<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-	<!--begin::Menu wrapper-->
-	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+	{{-- PENYELARAS UA --}}
+	@elseif(Auth::user()->tahap=='2')
+		@php
+			$user = DB::table('users')->where('no_kp',Auth::user()->no_kp)->first();
+			$institusi = DB::table('bk_info_institusi')->where('id_institusi', $user->id_institusi)->first();
+		@endphp
+		
 		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-		<!--begin:Menu item-->
+				<!--begin:Menu item-->
 			<div class="menu-item">
-				<a class="menu-link" href="{{ route('dashboard') }}">
-					<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
-					<span class="menu-title">Laman Utama</span>
-				</a>
-			</div>
-			<div class="menu-item pt-5">
-				<div class="menu-content">
-					<span class="menu-heading fw-bold text-uppercase fs-7">Permohonan</span>
+					<a class="menu-link" href="{{ route('dashboard') }}">
+						<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
+						<span class="menu-title">Laman Utama</span>
+					</a>
 				</div>
-			</div>
-			<div class="menu-item">
-				<a class="menu-link" href="{{ route('senarai.permohonanBaharu')}}">
-					<span class="menu-icon">{!! getIcon('wallet', 'fs-2') !!}</span>
-					<span class="menu-title">Baharu</span>
-				</a>
-			</div>
-			<div class="menu-item">
-				<a class="menu-link" href="{{ route('bkoku.sejarah.permohonan')}}">
-					<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
-					<span class="menu-title">Sejarah</span>
-				</a>
-			</div>
-			<div class="menu-item pt-5">
-					<div class="menu-content">
-						<span class="menu-heading fw-bold text-uppercase fs-7">Tuntutan</span>
-					</div>
-			</div>
-			<div class="menu-item">
-				<a class="menu-link" href="{{route('senarai.bkoku.tuntutanBaharu')}}">
-					<span class="menu-icon">{!! getIcon('wallet', 'fs-2') !!}</span>
-					<span class="menu-title">Baharu</span>
-				</a>
-			</div>
-			<div class="menu-item">
-				<a class="menu-link" href="{{route('bkoku.sejarah.tuntutan')}}">
-					<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
-					<span class="menu-title">Sejarah</span>
-				</a>
-			</div>
-			@if($institusi->jenis_institusi === 'UA')
 				<div class="menu-item pt-5">
 					<div class="menu-content">
-						<span class="menu-heading fw-bold text-uppercase fs-7">Penyaluran</span>
+						<span class="menu-heading fw-bold text-uppercase fs-7">Permohonan</span>
 					</div>
 				</div>
 				<div class="menu-item">
-					<a class="menu-link" href="{{ route('penyelaras.muat-turun.SPBB')}}">
-						<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
-						<span class="menu-title">Muat Turun SPBB</span>
+					<a class="menu-link" href="{{ route('senarai.permohonanBaharu')}}">
+						<span class="menu-icon">{!! getIcon('wallet', 'fs-2') !!}</span>
+						<span class="menu-title">Baharu</span>
 					</a>
 				</div>
 				<div class="menu-item">
-					<a class="menu-link" href="{{ route('penyelaras.muat-naik.SPBB')}}">
-						<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
-						<span class="menu-title">Muat Naik SPBB</span>
+					<a class="menu-link" href="{{ route('bkoku.sejarah.permohonan')}}">
+						<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
+						<span class="menu-title">Sejarah</span>
+					</a>
+				</div>
+				<div class="menu-item pt-5">
+						<div class="menu-content">
+							<span class="menu-heading fw-bold text-uppercase fs-7">Tuntutan</span>
+						</div>
+				</div>
+				<div class="menu-item">
+					<a class="menu-link" href="{{route('senarai.bkoku.tuntutanBaharu')}}">
+						<span class="menu-icon">{!! getIcon('wallet', 'fs-2') !!}</span>
+						<span class="menu-title">Baharu</span>
 					</a>
 				</div>
 				<div class="menu-item">
-					<a class="menu-link" href="{{ url('penyelaras/penyaluran/permohonan-tuntutan/layak') }}">
-						<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
-						<span class="menu-title">Senarai Pembayaran</span>
+					<a class="menu-link" href="{{route('bkoku.sejarah.tuntutan')}}">
+						<span class="menu-icon">{!! getIcon('search-list', 'fs-2') !!}</span>
+						<span class="menu-title">Sejarah</span>
 					</a>
 				</div>
-				<div class="menu-item">
-					<a class="menu-link" href="{{ route('penyelaras.senarai.dibayar') }}">
-						<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
-						<span class="menu-title">Keputusan Pembayaran</span>
-					</a>
-				</div>
+				@if($institusi->jenis_institusi === 'UA')
+					<div class="menu-item pt-5">
+						<div class="menu-content">
+							<span class="menu-heading fw-bold text-uppercase fs-7">Penyaluran</span>
+						</div>
+					</div>
+					<div class="menu-item">
+						<a class="menu-link" href="{{ route('penyelaras.muat-turun.SPBB')}}">
+							<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
+							<span class="menu-title">Muat Turun SPBB</span>
+						</a>
+					</div>
+					<div class="menu-item">
+						<a class="menu-link" href="{{ route('penyelaras.muat-naik.SPBB')}}">
+							<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
+							<span class="menu-title">Muat Naik SPBB</span>
+						</a>
+					</div>
+					<div class="menu-item">
+						<a class="menu-link" href="{{ url('penyelaras/penyaluran/permohonan-tuntutan/layak') }}">
+							<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
+							<span class="menu-title">Senarai Pembayaran</span>
+						</a>
+					</div>
+					<div class="menu-item">
+						<a class="menu-link" href="{{ route('penyelaras.senarai.dibayar') }}">
+							<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
+							<span class="menu-title">Keputusan Pembayaran</span>
+						</a>
+					</div>
 
-				<div class="menu-item pt-5">
-					<div class="menu-content">
-						<span class="menu-heading fw-bold text-uppercase fs-7">Kemaskini</span>
+					<div class="menu-item pt-5">
+						<div class="menu-content">
+							<span class="menu-heading fw-bold text-uppercase fs-7">Kemaskini</span>
+						</div>
 					</div>
-				</div>
-				<div class="menu-item">
-					<a class="menu-link" href="{{ route('maklumat.bank')}}">
-						<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
-						<span class="menu-title">Maklumat Bank Universiti</span>
-					</a>
-				</div>
-				<div class="menu-item">
-					<a class="menu-link" href="{{ route('senarai.pelajar.ua')}}">
-						<span class="menu-icon">{!! getIcon('user', 'fs-2') !!}</span>
-						<span class="menu-title">Institusi Pengajian</span>
-					</a>
-				</div>
-			@endif
+					<div class="menu-item">
+						<a class="menu-link" href="{{ route('maklumat.bank')}}">
+							<span class="menu-icon">{!! getIcon('file', 'fs-2') !!}</span>
+							<span class="menu-title">Maklumat Bank Universiti</span>
+						</a>
+					</div>
+					<div class="menu-item">
+						<a class="menu-link" href="{{ route('senarai.pelajar.ua')}}">
+							<span class="menu-icon">{!! getIcon('user', 'fs-2') !!}</span>
+							<span class="menu-title">Institusi Pengajian</span>
+						</a>
+					</div>
+				@endif
+			</div>
 		</div>
 		<!--end::Menu-->
 
-@elseif(Auth::user()->tahap=='3')
-@php
-	$baharu = DB::table('permohonan')->where('status', '=', '2')->count();
-	$sokong = DB::table('permohonan')->where('status', '=', '4')->count();
-	// $keputusan = DB::table('permohonan')->whereIn('status', ['6', '7'])->count();
-	$layak = DB::table('permohonan')->where('status', '=', '6')->count();
-	$bayar = DB::table('permohonan')->where('status', '=', '8')->count();
-	$total = DB::table('permohonan')->count();
-	$baharuT = DB::table('tuntutan')->where('status', '=', '2')->count();
-	// $keputusanT = DB::table('tuntutan')->whereIn('status', ['6', '7'])->count();
-	$layakT = DB::table('tuntutan')->where('status', '=', '6')->count();
-	$bayarT = DB::table('tuntutan')->where('status', '=', '8')->count();
-	$totalT = DB::table('tuntutan')->count();
-@endphp
-<!--begin::sidebar menu-->
-<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-	<!--begin::Menu wrapper-->
-	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+	{{-- SEKRETARIAT --}}
+	@elseif(Auth::user()->tahap=='3')
+		@php
+			$baharu = DB::table('permohonan')->where('status', '=', '2')->count();
+			$sokong = DB::table('permohonan')->where('status', '=', '4')->count();
+			// $keputusan = DB::table('permohonan')->whereIn('status', ['6', '7'])->count();
+			$layak = DB::table('permohonan')->where('status', '=', '6')->count();
+			$bayar = DB::table('permohonan')->where('status', '=', '8')->count();
+			$total = DB::table('permohonan')->count();
+			$baharuT = DB::table('tuntutan')->where('status', '=', '2')->count();
+			// $keputusanT = DB::table('tuntutan')->whereIn('status', ['6', '7'])->count();
+			$layakT = DB::table('tuntutan')->where('status', '=', '6')->count();
+			$bayarT = DB::table('tuntutan')->where('status', '=', '8')->count();
+			$totalT = DB::table('tuntutan')->count();
+		@endphp
+
 		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
 			<!--begin:Menu item-->
@@ -379,14 +397,11 @@
 		</div>
 		<!--end::Menu-->
 
-@elseif(Auth::user()->tahap=='4')
-<!--begin::sidebar menu-->
-<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-	<!--begin::Menu wrapper-->
-	<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+	{{-- PEGAWAI --}}
+	@elseif(Auth::user()->tahap=='4')
 		<!--begin::Menu-->
 		<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-		<!--begin:Menu item-->
+			<!--begin:Menu item-->
 			<div class="menu-item">
 				<a class="menu-link" href="{{ route('dashboard') }}">
 					<span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
@@ -410,16 +425,9 @@
 					<span class="menu-title">Tuntutan</span>
 				</a>
 			</div>
-
-
 		</div>
 
-@elseif(Auth::user()->tahap=='5')
-
-		<!--begin::sidebar menu-->
-		<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
-			<!--begin::Menu wrapper-->
-			<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+	@elseif(Auth::user()->tahap=='5')
 				<!--begin::Menu-->
 				<div class="menu menu-column menu-rounded menu-sub-indention px-3 fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
 				<!--begin:Menu item-->
@@ -465,11 +473,10 @@
 								<span class="menu-title">Selenggara Had Maksima Tuntutan</span>
 						</a>
 					</div>
-
-
 				</div>
 			<!--end::Menu-->
-			@elseif(Auth::user()->tahap=='6')
+
+@elseif(Auth::user()->tahap=='6')
 <!--begin::sidebar menu-->
 <div class="app-sidebar-menu overflow-hidden flex-column-fluid">
 	<!--begin::Menu wrapper-->
@@ -531,11 +538,22 @@
 			</div>
 		</div>
 		<!--end::Menu-->
-
-
 @endif
 
 	</div>
 	<!--end::Menu wrapper-->
 </div>
 <!--end::sidebar menu-->
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script>
+	function showAlert() 
+	{
+		Swal.fire({
+		icon: 'error',
+		title: 'Permohonan ditutup pada masa sekarang.',
+		text: ' {!! session('failed') !!}',
+		confirmButtonText: 'OK'
+		});
+	}
+</script>
