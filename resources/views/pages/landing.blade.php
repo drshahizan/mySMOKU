0b04bb461701a32a41c31317e592d51e37471a87
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-02-06 23:23:45 +0800

    Update landing.blade.php

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -852,11 +852,9 @@
 		<!--end::Global Javascript Bundle-->
 		<!--begin::Vendors Javascript(used for this page only)-->
 		<script src="assets/plugins/custom/fslightbox/fslightbox.bundle.js"></script>
-		<script src="assets/plugins/custom/typedjs/typedjs.bundle.js"></script>
 		<!--end::Vendors Javascript-->
 		<!--begin::Custom Javascript(used for this page only)-->
 		<script src="assets/js/custom/landing.js"></script>
-		<script src="assets/js/custom/pages/pricing/general.js"></script>
 		<!--end::Custom Javascript-->
 		<!--end::Javascript-->
 	</body>
