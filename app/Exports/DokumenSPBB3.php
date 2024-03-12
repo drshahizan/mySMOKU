5ada75009699a21d2213eb04eef4bb2baf6d39d7
Fateen Nashuha <92704116+fateen911@users.noreply.github.com>
2024-03-12 11:11:58 +0800

    Update DokumenSPBB3.php

--- app/Exports/DokumenSPBB3.php
+++ app/Exports/DokumenSPBB3.php
@@ -182,7 +182,7 @@ class DokumenSPBB3 implements FromCollection, WithHeadings, WithEvents, WithMapp
         $currentYear = Carbon::now()->year;
 
         // Determine the sesi_bayaran based on the current month and year
-        if ($currentMonth == 2) {
+        if ($currentMonth == 3) {
             $sesiBayaran = '1 ' . $currentYear . '/'. $currentYear+1;
         } elseif ($currentMonth == 4) {
             $sesiBayaran = '2 ' . $currentYear . '/'. $currentYear+1;
