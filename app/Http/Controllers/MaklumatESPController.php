8743f990817ee9d2e92102cbf95a6b4d3779bbd2
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-03-20 11:47:15 +0800

    Update MaklumatESPController.php

--- app/Http/Controllers/MaklumatESPController.php
+++ app/Http/Controllers/MaklumatESPController.php
@@ -21,7 +21,7 @@ class MaklumatESPController extends Controller
 {
     public function permohonan()
     {
-        $kelulusan = Permohonan::orderBy('tarikh_hantar', 'desc')->where('status', '=','6')->get();
+        $kelulusan = Permohonan::orderBy('tarikh_hantar', 'desc')->where('status', '=','6')->where('data_migrate', '!=','1')->get();
 
         $secretKey = '2z_JoT4dDCNe_bkT9y6kEhc_4plRkUW7Ci1hzoyH';
 
