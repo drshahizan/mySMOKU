ba11c109a2a17dd4dad7ad34caf8f2c829fb4c2e
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-08-06 01:00:21 +0800

    hebahan

--- app/Http/Controllers/Auth/LandingPageController.php
+++ app/Http/Controllers/Auth/LandingPageController.php
@@ -11,8 +11,8 @@ class LandingPageController extends Controller
     public function index()
     {   
         $iklan = TarikhIklan::orderBy('created_at', 'desc')->first();
-        $catatan = $iklan ? $iklan->catatan : "";
+        $hebahan = $iklan ? $iklan->catatan : "";
         // Log::info('Catatan: ' . $catatan); // Log the content of $catatan
-        return view('pages.landing', compact('catatan'));// CATATAN
+        return view('pages.landing',compact('hebahan'));// CATATAN
     }   
 }
