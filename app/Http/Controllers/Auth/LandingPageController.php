6315b1e152242adca656ccfac416d7643fbd37e7
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-08-06 00:19:43 +0800

    Update LandingPageController.php

--- app/Http/Controllers/Auth/LandingPageController.php
+++ app/Http/Controllers/Auth/LandingPageController.php
@@ -1,24 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
-use App\Models\Hubungan;
-use App\Models\JenisOku;
-use App\Models\Keturunan;
-use Illuminate\Http\Request;
-use App\Models\Smoku;
 use App\Models\TarikhIklan;
-use DateTime;
-use GuzzleHttp\Client;
-use Illuminate\Support\Facades\DB;
+
 
 class LandingPageController extends Controller
 {
     public function index()
     {   
         $iklan = TarikhIklan::orderBy('created_at', 'desc')->first();
-        $catatan = $iklan->catatan ?? "";
-        return view('pages.landing', compact('catatan'));// CATATAN
+        $catatan = $iklan ? $iklan->catatan : "";
+        return view('pages.landing', compact('catatan'));
     }   
 }
