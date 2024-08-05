538e93cee3557cbcba3bf08d96056704d07232c7
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-08-06 00:29:08 +0800

    Update 2023_10_21_230932_create_maklumat_bank_table.php

--- database/migrations/2023_10_21_230932_create_maklumat_bank_table.php
+++ database/migrations/2023_10_21_230932_create_maklumat_bank_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('maklumat_bank', function (Blueprint $table) {
             $table->id();
-            $table->string('institusi_id');
+            $table->unsignedBigInteger('institusi_id');
             $table->string('bank_id');
             $table->string('nama_akaun');
             $table->string('no_akaun');
             $table->string('penyata_bank');
-            // $table->foreign('institusi_id')
-            //   ->references('id_institusi')->on('bk_info_institusi')->onDelete('cascade');
+            $table->foreign('institusi_id')
+              ->references('id_institusi')->on('bk_info_institusi')->onDelete('cascade');
             $table->timestamps();
         });
     }
