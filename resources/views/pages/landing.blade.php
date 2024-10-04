931d8c36574f8c1ec66d3a7f6a89e69f2336270e
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-10-04 23:03:33 +0800

    Update landing.blade.php

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1580,11 +1580,11 @@
 			<!--begin::Footer Section-->
 			<div class="mb-0">
 				<!--begin::Curve top-->
-				<div class="landing-curve landing-dark-color" style="color: rgb(2, 2, 55);">
+				{{-- <div class="landing-curve landing-dark-color" style="color: rgb(2, 2, 55);">
 					<svg viewBox="15 -1 1470 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M1 48C4.93573 47.6644 8.85984 47.3311 12.7725 47H1489.16C1493.1 47.3311 1497.04 47.6644 1501 48V47H1489.16C914.668 -1.34764 587.282 -1.61174 12.7725 47H1V48Z" fill="currentColor"></path>
 					</svg>
-				</div>
+				</div> --}}
 				<!--end::Curve top-->
 
 				<!--begin::Wrapper-->
