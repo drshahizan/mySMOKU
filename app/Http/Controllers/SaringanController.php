32431527507049ae991f3c623b71a034736c3819
Wansyafiqah <wnsyafiqah4@gmail.com>
2024-10-15 10:01:38 +0800

    Update SaringanController.php

--- app/Http/Controllers/SaringanController.php
+++ app/Http/Controllers/SaringanController.php
@@ -263,7 +263,60 @@ class SaringanController extends Controller
         $status_kod = 3;
         $status = "Permohonan dan tuntutan ".$no_rujukan_permohonan." telah disaring dan disokong.";
 
-        return view('permohonan.sekretariat.saringan.senarai_permohonan',compact('permohonan','status_kod','status', 'institusiPengajianIPTS', 'institusiPengajianPOLI', 'institusiPengajianKK', 'institusiPengajianUA','institusiPengajianPPK'));
+
+        //11102024
+        $institusiPengajianIPTS = InfoIpt::where('jenis_institusi', 'IPTS')->orderBy('nama_institusi')->get();
+        $institusiPengajianPOLI = InfoIpt::where('jenis_institusi','P')->orderBy('nama_institusi')->get();
+        $institusiPengajianKK = InfoIpt::where('jenis_institusi','KK')->orderBy('nama_institusi')->get();
+        $institusiPengajianUA = InfoIpt::where('jenis_institusi','UA')->orderBy('nama_institusi')->get();
+        $institusiPengajianPPK = InfoIpt::where('id_institusi', '01055')->orWhere('jenis_permohonan', 'PPK')->orderBy('nama_institusi')->get();
+
+        // Extract ID values from the collections
+        $idsIPTS = $institusiPengajianIPTS->pluck('id_institusi')->toArray();
+        $idsPOLI = $institusiPengajianPOLI->pluck('id_institusi')->toArray();
+        $idsKK = $institusiPengajianKK->pluck('id_institusi')->toArray();
+        $idsUA = $institusiPengajianUA->pluck('id_institusi')->toArray();
+        $idsPPK = $institusiPengajianPPK->pluck('id_institusi')->toArray();
+
+        // Count the number of applications for each institution type with smoku_akademik join
+        $countUA = Permohonan::join('smoku_akademik', 'permohonan.smoku_id', '=', 'smoku_akademik.smoku_id')
+                            ->where('smoku_akademik.status', 1)
+                            ->where('permohonan.program', 'BKOKU')
+                            ->whereIn('smoku_akademik.id_institusi', $idsUA)
+                            ->whereIn('permohonan.status', ['2'])
+                            ->count();               
+
+        $countPOLI = Permohonan::join('smoku_akademik', 'permohonan.smoku_id', '=', 'smoku_akademik.smoku_id')
+                            ->where('smoku_akademik.status', 1)
+                            ->where('permohonan.program', 'BKOKU')
+                            ->whereIn('smoku_akademik.id_institusi', $idsPOLI)
+                            ->whereIn('permohonan.status', ['2'])
+                            ->count();
+
+        $countKK = Permohonan::join('smoku_akademik', 'permohonan.smoku_id', '=', 'smoku_akademik.smoku_id')
+                            ->where('smoku_akademik.status', 1)
+                            ->where('permohonan.program', 'BKOKU')
+                            ->whereIn('smoku_akademik.id_institusi', $idsKK)
+                            ->whereIn('permohonan.status', ['2'])
+                            ->count();
+
+        $countIPTS = Permohonan::join('smoku_akademik', 'permohonan.smoku_id', '=', 'smoku_akademik.smoku_id')
+                            ->where('smoku_akademik.status', 1)
+                            ->where('permohonan.program', 'BKOKU')
+                            ->whereIn('smoku_akademik.id_institusi', $idsIPTS)
+                            ->whereIn('permohonan.status', ['2'])
+                            ->count();
+
+        $countPPK = Permohonan::join('smoku_akademik', 'permohonan.smoku_id', '=', 'smoku_akademik.smoku_id')
+                            ->where('smoku_akademik.status', 1)
+                            ->whereIn('smoku_akademik.id_institusi', $idsPPK)
+                            ->whereIn('permohonan.status', ['2'])
+                            ->count();
+
+        // Debug output
+        // dd($countUA, $countPOLI, $countKK, $countIPTS, $countPPK);
+
+        return view('permohonan.sekretariat.saringan.senarai_permohonan',compact('permohonan','status_kod','status', 'institusiPengajianIPTS', 'institusiPengajianPOLI', 'institusiPengajianKK', 'institusiPengajianUA','institusiPengajianPPK', 'countIPTS', 'countPOLI', 'countKK', 'countUA', 'countPPK'));
     }
 
     public function saringPermohonan(Request $request,$id)
