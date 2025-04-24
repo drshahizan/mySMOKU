c64643baeb8462ec9f24d161699eae1b457eaf94
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-04-24 16:45:22 +0800

    Update surat_tawaran.blade.php

--- resources/views/permohonan/sekretariat/keputusan/surat_tawaran.blade.php
+++ resources/views/permohonan/sekretariat/keputusan/surat_tawaran.blade.php
@@ -5,7 +5,7 @@
                 size: A4;
                 font-family: Arial, sans-serif;
                 font-size: 12pt;
-                line-height: 1.15;
+                line-height: 1.5;
             }
     
             body::before {
@@ -35,6 +35,7 @@
             .parentSpace {
                 width: 100%;
                 display: inherit;
+                line-height: 1.0;
             }
     
             .left {
@@ -44,8 +45,8 @@
     
             .right {
                 float: right;
-                width: 20%;
-                font-size: 9pt; 
+                width: 30%;
+                font-size: 11pt; 
                 margin-top: 100px;
                 line-height: 1;
             }
@@ -56,19 +57,18 @@
         <div class="parentSpace">
             <div class="left">
                 <div class="logo" style="margin-top:20px; float: left;">
-                    <img src="logoKPT.png" alt="Kementerian Pengajian Tinggi" style="height: 100px; width: 150px;">
+                    <img src="logoKPT.png" alt="Kementerian Pengajian Tinggi" style="height: 110px; width: 160px;">
                 </div>
 
-                <div class="address" style="padding-left: 150px; font-size: 10pt; margin-top:0%;">
-                    <b>{{ strtoupper ($maklumat_kementerian->nama_kementerian_bm)}}</b>
-                    <br><span style="font-style: italic;">{{ strtoupper($maklumat_kementerian->nama_kementerian_bi) }}</span>
-                    <br>
-                    <br>{{$maklumat_kementerian->nama_bahagian_bm}}
-                    <br><span style="font-style: italic;">{{$maklumat_kementerian->nama_bahagian_bi}}</span>
+                <div class="address" style="padding-left: 170px; font-size: 11pt; margin-top:15px;">
+                    <b>{{ strtoupper($maklumat_kementerian->nama_kementerian_bm) }}</b>
+                    <br><b style="font-style: italic;">{{ strtoupper($maklumat_kementerian->nama_kementerian_bi) }}</b>
+                    <br><b>{{$maklumat_kementerian->nama_bahagian_bm}}</b>
+                    <br><b style="font-style: italic;">{{$maklumat_kementerian->nama_bahagian_bi}}</b>
                     <br>{{$maklumat_kementerian->alamat1}}
                     <br>{{$maklumat_kementerian->alamat2}}
-                    <br>{{$maklumat_kementerian->poskod}} {{$maklumat_kementerian->negeri}}
-                    <br>{{$maklumat_kementerian->negara}}
+                    <br>{{$maklumat_kementerian->poskod}} {{strtoupper($maklumat_kementerian->negeri)}}
+                    <br>{{strtoupper($maklumat_kementerian->negara)}}
                 </div>
             </div>
 
@@ -80,20 +80,15 @@
                         <td>{{$maklumat_kementerian->tel}}</td>
                     </tr>
                     <tr>
-                        <td>Hotline</td>
+                        <td>E-mel</td>
                         <td>:</td>
-                        <td>{{$maklumat_kementerian->hotline}}</td>
-                    </tr>
-                    <tr>
-                        <td>Faks</td>
-                        <td>:</td>
-                        <td>{{$maklumat_kementerian->faks}}</td>
+                        <td>{{$maklumat_kementerian->email}}</td>
                     </tr>
                 </table>
             </div>
         </div>
            
-        <br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br>
         
         <hr>
 
@@ -109,6 +104,7 @@
             $tarikh_mula = DB::table('smoku_akademik')->where('smoku_id', $permohonan['smoku_id'])->value('tarikh_mula');
             $tarikh_tamat = DB::table('smoku_akademik')->where('smoku_id', $permohonan['smoku_id'])->value('tarikh_tamat');
             $mod = DB::table('smoku_akademik')->join('bk_mod','bk_mod.kod_mod','=','smoku_akademik.mod' )->where('smoku_id', $permohonan['smoku_id'])->value('bk_mod.mod');
+            $peringkat = DB::table('smoku_akademik')->join('bk_peringkat_pengajian','bk_peringkat_pengajian.kod_peringkat','=','smoku_akademik.peringkat_pengajian' )->where('smoku_id', $permohonan['smoku_id'])->value('bk_peringkat_pengajian.peringkat');
             $institusi = DB::table('smoku_akademik')->join('bk_info_institusi','bk_info_institusi.id_institusi','=','smoku_akademik.id_institusi' )->where('smoku_id', $permohonan['smoku_id'])->value('bk_info_institusi.nama_institusi');
         @endphp
 
@@ -116,91 +112,105 @@
             \Carbon\Carbon::setLocale('ms');
         @endphp
 
-        <p>
-            <span style="float: right">
-                @if ($permohonan->program == "BKOKU")
-                    Ruj. Kami : KPT.BKOKU-{{$no_kp}}<br>
-                @else
-                    Ruj. Kami : KPT.PPK-{{$no_kp}}<br>
-                @endif
-                Tarikh : {{date('d/m/Y', strtotime($tarikh_kelulusan))}}<br>
-            </span>
-        </p>
+        <table style="float: right">
+            <tr>
+                <td>Ruj. Kami</td>
+                <td>:</td>
+                <td>KPT.BKOKU-{{$no_kp}}</td>
+            </tr>
+            <tr>
+                <td>Tarikh</td>
+                <td>:</td>
+                <td> {{date('d/m/Y', strtotime($tarikh_kelulusan))}}</td>
+            </tr>
+        </table>
 
-        <br><br>
+        <br><br><br>
 
         <div class="penerima" style="width: 50%">
             <b>{{$nama}}</b>
-            <br><b>NO.KP : {{$no_kp}}</b>
-            <br>{{$alamat}},
-            <br>{{$poskod}}, {{$bandar}}
+            <br><b>{{$no_kp}}</b>
+            <br>{{$alamat}}
+            <br>{{$poskod}} {{$bandar}}
             <br>{{$negeri}}
         </div>
             
         <br>
         <p>Tuan/Puan,</p>
-        <br>
-        <h3>{{ strtoupper($kandungan_surat->tajuk) }}</h3>
-        <br>
-        <p>{{ $kandungan_surat->tujuan }}</p>
+        
+        <p><b>{{ strtoupper($kandungan_surat->tajuk) }}</b></p>
+        <p>{{$kandungan_surat->hormat}}</p>
+        <p>{{$kandungan_surat->tujuan }}</p>
         <br>
 
         <table>
             <tr>
-                <td><strong>Program Pengajian </strong></td>
+                <td><strong>PERINGKAT</strong></td>
                 <td><b>:</b></td>
-                <td>{{ strtoupper($program) }}</td>
+                <td>{{ strtoupper($peringkat) }}</td>
             </tr>
 
             <tr>
-                <td><strong>Mod Pengajian </strong></td>
+                <td><strong>KURSUS </strong></td>
                 <td><b>:</b></td>
-                <td>{{strtoupper($mod)}}</td>
+                <td>{{strtoupper($program)}}</td>
             </tr>
 
             <tr>
-                <td><strong>Tempoh Penajaan </strong></td>
-                <td><b>:</b></td>
-                <td>{{ \Carbon\Carbon::parse($tarikh_mula)->isoFormat('D MMMM Y') }} hingga {{ \Carbon\Carbon::parse($tarikh_tamat)->isoFormat('D MMMM Y') }}</td>
-            </tr>
-
-            <tr>
-                <td><strong>Institusi Pengajian </strong></td>
+                <td><strong>INSTITUSI</strong></td>
                 <td><b>:</b></td>
                 <td>{{strtoupper($institusi)}}</td>
+            </tr>
+
+            <tr>
+                <td><strong>TEMPOH PENGAJIAN </strong></td>
+                <td><b>:</b></td>
+                <td>{{ \Carbon\Carbon::parse($tarikh_mula)->isoFormat('D MMMM Y') }} HINGGA {{ \Carbon\Carbon::parse($tarikh_tamat)->isoFormat('D MMMM Y') }}</td>
+            </tr>
+
+            <tr>
+                <td><strong>MOD PENGAJIAN </strong></td>
+                <td><b>:</b></td>
+                <td>{{strtoupper($mod)}}</td>
             </tr>
         </table>
         
         <br>
-        <div class="main-content" style="text-align:justify;">
-            <p>2.  Bantuan ini berkuatkuasa mulai <b>{{ \Carbon\Carbon::parse($tarikh_mula)->isoFormat('D MMMM Y') }} hingga {{ \Carbon\Carbon::parse($tarikh_tamat)->isoFormat('D MMMM Y') }}.</b>
-                  {{$kandungan_surat->kandungan1}}</p><br>
-            <p>3.   {{$kandungan_surat->kandungan2}}</p><br>
-            <p>4.   {{$kandungan_surat->kandungan3}}</p><br>
+        <div class="main-content" style="text-align: justify;">
+            <p><b>2. TEMPOH KUAT KUASA</b>
+                <br>
+                {{$kandungan_surat->kandungan1}}</p>
+            <p><b>3. KADAR PEMBIAYAAN</b>
+                <br>{{$kandungan_surat->kandungan2}}</p>
+            <p><b>4. HAK KERAJAAN</b>
+                <br>{{$kandungan_surat->kandungan3}}</p>
         </div>
         
+        <br>
         <p>Sekian, terima kasih.</p>
         <br>
-        <p><b>{{$kandungan_surat->penutup1}}</b></p>
-        <br>
+        <p><b>{{ $kandungan_surat->penutup1}}</b></p>
         <p><b>{{$kandungan_surat->penutup4_4}}</b></p>
+        <p><b>{{ $kandungan_surat->penutup2}}</b></p>
+        <p>Saya yang menjalankan amanah,</p>
         <br>
-        <p><b>{{$kandungan_surat->penutup2}}</b></p>
-        
-        <p> Saya yang menjalankan amanah,</p><br>
-        <p> {{$kandungan_surat->penutup3_1}} <br>
+        <p>
+            <b>{{$kandungan_surat->penutup3_1}}</b> <br>
             {{$kandungan_surat->penutup3_2}} <br>
             {{$kandungan_surat->penutup3_3}} <br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             {{$kandungan_surat->penutup3_4}} <br>
+            
         </p>
-        <br><br>
-        <div style="text-align: center; font-style: italic;">"Nota: Surat ini adalah cetakan komputer dan tandatangan tidak diperlukan."</div>
-        <br><br>
-        <p>s.k :<br>
-            {{$kandungan_surat->penutup4_1}} <br>
-            {{$kandungan_surat->penutup4_2}} <br>
+        <br>
+        <p>s.k : {{$kandungan_surat->penutup4_1}} <br>
             {{strtoupper($institusi)}} <br>
         </p>
+        <br>
+        <p style="text-align: center;">
+            <span style="font-style: italic;"><b>
+              * Surat ini adalah cetakan komputer dan tandatangan tidak diperlukan.
+            </b></span>
+        </p>
+        
     </body>
 </html>
