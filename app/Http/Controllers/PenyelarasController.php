f08777d6f62285e009789713bda8e611e15561a6
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-07-03 17:31:37 +0800

    Update PenyelarasController.php

--- app/Http/Controllers/PenyelarasController.php
+++ app/Http/Controllers/PenyelarasController.php
@@ -1050,7 +1050,7 @@ class PenyelarasController extends Controller
             $sesiSemasa = null; // Initialize a variable to store the current session
 
             foreach ($nextSemesterDates as $key => $data) {
-                echo 'Date: ' . $data['date'] . ', Semester: ' . $data['semester'] . ', Sesi: ' . $data['sesi'] . '<br>';
+                // echo 'Date: ' . $data['date'] . ', Semester: ' . $data['semester'] . ', Sesi: ' . $data['sesi'] . '<br>';
                 
 
                 $dateOfSemester = \Carbon\Carbon::parse($data['date']);
@@ -1078,7 +1078,7 @@ class PenyelarasController extends Controller
             // echo 'Previous Session: ' . $previousSesi . PHP_EOL;
             // echo 'Current Semester: ' . $semSemasa . PHP_EOL;
             // echo 'Current Session: ' . $sesiSemasa . PHP_EOL;
-             dd('sini');
+            //  dd('sini');
             // dd($semesterEndDate);
 
             if ($currentDate <= $semesterEndDate ) {
