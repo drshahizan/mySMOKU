b479ee1e5bddf538491939362153ccd9aaf59f81
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-08-18 11:11:39 +0800

    Update papar_tuntutan.blade.php

--- resources/views/tuntutan/sekretariat/sejarah/papar_tuntutan.blade.php
+++ resources/views/tuntutan/sekretariat/sejarah/papar_tuntutan.blade.php
@@ -190,7 +190,7 @@
                                                     <tr>
                                                         <td style="text-align:right;">1</td>
                                                         <td>
-                                                            <span><a href="{{ url('tuntutan/sekretariat/saringan/keputusan-peperiksaan') }}" target="_blank">Keputusan Peperiksaan</a></span>
+                                                            <span><a href="{{ url('tuntutan/sekretariat/saringan/keputusan-peperiksaan/'.$permohonan->id) }}" target="_blank">Keputusan Peperiksaan</a></span>
                                                         </td>
                                                         <td>
                                                             -
