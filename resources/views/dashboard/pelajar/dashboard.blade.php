5a72d2ad0eaefc730ce4b97bde8eceed8a8d688b
Wansyafiqah <wnsyafiqah4@gmail.com>
2025-11-28 11:17:43 +0800

    Update dashboard.blade.php

--- resources/views/dashboard/pelajar/dashboard.blade.php
+++ resources/views/dashboard/pelajar/dashboard.blade.php
@@ -4,7 +4,6 @@
    {{-- <link rel="stylesheet" href="/assets/css/saringan.css"> --}}
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-   <script src="/assets/lang/Malay.json"></script>
    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 </head>
 	<!--begin::Page title-->
