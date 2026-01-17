a8431915148158152ab0e66fdf09b7821fdfb1af
Wansyafiqah <wnsyafiqah4@gmail.com>
2026-01-18 00:06:09 +0800

    Update surat_tawaran.blade.php

--- resources/views/kemaskini/pelajar/surat_tawaran.blade.php
+++ resources/views/kemaskini/pelajar/surat_tawaran.blade.php
@@ -12,7 +12,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-        <script src="/assets/lang/Malay.json"></script>
         <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
     </head>
 
