4d1b1394e6b6edeef631e810c0f8a740728b7ea2
Wansyafiqah <wnsyafiqah4@gmail.com>
2026-01-18 17:58:37 +0800

    middleware untuk set header no-cache bila user sudah login, jadi lepas logout browser tak boleh “back” ke page sebelumnya sebab response dulu sudah no-store

---
+++ app/Http/Middleware/NoCacheIfAuthenticated.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoCacheIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (Auth::check()) {
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', '0');
+        }
+
+        return $response;
+    }
+}
