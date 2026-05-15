6870754360f023ba7b1275d415820fcb631a991f
Wansyafiqah <wnsyafiqah4@gmail.com>
2026-05-15 15:28:37 +0800

    Update hebahan_iklan.blade.php

--- resources/views/kemaskini/pentadbir/hebahan_iklan.blade.php
+++ resources/views/kemaskini/pentadbir/hebahan_iklan.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>SistemBKOKU: HEBAHAN IKLAN PERMOHONAN BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)/ PROGRAM PENDIDIKAN KHAS (PPK)</title>
+    <title>SisPO: HEBAHAN IKLAN PERMOHONAN BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)/ PROGRAM PENDIDIKAN KHAS (PPK)</title>
 </head>
 <body>
     <h3>HEBAHAN IKLAN PERMOHONAN BANTUAN KEWANGAN ORANG KURANG UPAYA (BKOKU)/ PROGRAM PENDIDIKAN KHAS (PPK)</h3>
@@ -15,13 +15,13 @@
     <br>
     <br>
     <br>
-    <p>Jika mempunyai sebarang pertanyaan, sila hubungi <i>hotline</i> BKOKU di talian 03-8888 1616 atau e-mel ke bkoku@mohe.gov.my. </p>
+    <p>Jika mempunyai sebarang pertanyaan, sila hubungi <i>hotline</i> BKOKU di talian 03-8870 6347 atau e-mel ke sispo@mohe.gov.my. </p>
    
-    <p>Untuk maklumat lanjut, sila layari https://biasiswa.mohe.gov.my/ </p>
+    <p>Untuk maklumat lanjut, sila layari https://bkoku.mohe.gov.my </p>
     <br>
     <p>Sekian, terima kasih.</p>
     <br>
-    <p><b>Unit Bantuan Kewangan OKU/Program Pendidikan Khas</b></p>
+    <p><b>Unit Bantuan Kewangan OKU / Program Pendidikan Khas</b></p>
     <p><b>Bahagian Biasiswa</b></p>
     <p><b>Kementerian Pendidikan Tinggi</b></p>
 </body>
